73f6f1f13073005fecf277e7f2fa0e233036b1c6
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-04-11 23:09:13 +0300

    Args

---
+++ final_task/pycalc/args.py
@@ -0,0 +1,37 @@
+import argparse
+
+PARSER = {
+    'description': 'Pure-python command-line calculator.'
+}
+
+EXPRESSION = {
+    'name_or_flags': ['expression'],
+    'keyword_arguments': {
+        'metavar': 'EXPRESSION',
+        'type': str,
+        'help': 'expression string to evaluate'
+    }
+}
+
+MODULE = {
+    'name_or_flags': ['-m', '--use-modules'],
+    'keyword_arguments': {
+        'metavar': 'MODULE',
+        'type': str,
+        'nargs': '+',
+        'help': 'additional modules to use',
+        'dest': 'modules'
+    }
+}
+
+arguments = [
+    EXPRESSION,
+    MODULE,
+]
+
+parser = argparse.ArgumentParser(**PARSER)
+
+for arg in arguments:
+    parser.add_argument(*arg['name_or_flags'], **arg['keyword_arguments'])
+
+args = parser.parse_args()
