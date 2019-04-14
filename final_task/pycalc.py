17c503098ca8f8c86e397bb7cdce598306da6c7b
Nikita <47901500+Ronin-niron@users.noreply.github.com>
2019-04-14 19:22:34 +0300

    first_comit

---
+++ final_task/pycalc.py
@@ -0,0 +1,227 @@
+OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y),
+             '%': (3, lambda x, y: x % y)}
+
+
+line = input()
+z = 0
+numbers = "0123456789."
+mat = ('+', '-', '/', '*', '//', '**', '^', '%')
+letters = "abcdefghjklmnopqrstuvwxyz &$#@абвгдежзиклмнопрстухцчюшщуыывюяё"
+result = 0
+
+
+def matched(str):
+    count = 0
+    for i in str:
+        if i == "(":
+            count += 1
+        elif i == ")":
+            count -= 1
+        if count < 0:
+            print('opening bracket not found')
+            quit()
+    return count == 0
+
+
+matched(line)
+
+
+def only_letters(tested_string):
+    for sign in tested_string:
+        if sign not in letters:
+            continue
+        else:
+            print("In the entered expression are not only numbers and math.")
+            quit()
+
+
+only_letters(line)
+
+
+if line[0] in mat:
+    print('The first value cannot be early ' + line[0])
+    quit()
+if line[-1] in mat:
+    print('The last value cannot be early ' + line[-1])
+    quit()
+if line[0] == ')':
+    print('The first value cannot be early ' + line[0])
+    quit()
+if line[-1] == '(':
+    print('The last value cannot be early ' + line[-1])
+    quit()
+
+
+for bracket in line:
+    if bracket == '(':
+        x = line.index('(')
+        y = line[x + 1]
+        if y in mat:
+            print('after the sign ( must be a number')
+            quit()
+        elif line.find(')') == -1:
+            print('a character was entered \'(\' but the character was not entered \')\'')
+            quit()
+        else:
+            continue
+
+
+if set(line).isdisjoint(mat):
+    print('The entered expression does not contain mathematical operations.')
+    quit()
+
+
+def split_operators(s):
+    doker = []
+    last_number = ""
+    for c in s:
+        if c in numbers:
+            last_number += c
+        else:
+            if last_number:
+                doker.append(last_number)
+                last_number = ""
+            if c:
+                doker.append(c)
+    if last_number:
+        doker.append(last_number)
+    return doker
+
+
+line = split_operators(line)
+
+
+for idx, stack in enumerate(line):
+    if stack == '(':
+        if idx == 0:
+            continue
+        elif line[idx - 1] in mat:
+            continue
+        elif line[idx - 1] in numbers or line[idx - 1] == stack:
+            inpt_star = idx - 1
+            line.insert(idx, '*')
+            continue
+        else:
+            continue
+    elif stack == ')':
+        out = len(line)
+        if idx == out - 1:
+            break
+        elif line[idx + 1] in mat:
+            continue
+        elif line[idx + 1] in numbers or line[idx + 1] == '(':
+            inpt_star = idx + 1
+            line.insert(inpt_star, '*')
+            continue
+        elif line[idx + 1] == stack:
+            continue
+        else:
+            continue
+
+
+line = "".join(str(item) for item in line)
+
+
+for idx, stack in enumerate(line):
+    if stack == '*':
+        if line[idx + 1] == stack:
+            x = line[idx - 1]
+            x_ind = idx - 1
+            y = line[idx + 2]
+            y_ind = idx + 2
+            if y == stack:
+                print('not known mathematical action ***')
+                quit()
+            elif y in mat:
+                print('not known mathematical action **' + y)
+                quit()
+            else:
+                z = int(x) ** int(y)
+                line = split_operators(line)
+                line[x_ind:y_ind+1] = [z]
+                break
+        elif line[idx + 1] != stack:
+            continue
+
+
+line = "".join(str(item) for item in line)
+
+
+for idx, stack in enumerate(line):
+    if stack == '/':
+        if line[idx + 1] == stack:
+            x = line[idx - 1]
+            x_ind = idx - 1
+            y = line[idx + 2]
+            y_ind = idx + 2
+            if y == stack:
+                print('not known mathematical action ///')
+                quit()
+            else:
+                z = int(x) // int(y)
+                line = split_operators(line)
+                line[x_ind:y_ind+1] = [z]
+                break
+        elif line[idx + 1] != stack:
+            continue
+
+
+line = "".join(str(item) for item in line)
+
+
+def eval_(formula):
+    def parse(formula_string):
+        number = ''
+        for s in formula_string:
+            if s in '1234567890.':
+                number += s
+            elif number:
+                yield float(number)
+                number = ''
+            if s in OPERATORS or s in "()":
+                yield s
+        if number:
+            yield float(number)
+
+    def shunting_yard(parsed_formula):
+        stack = []
+        for token in parsed_formula:
+            if token in OPERATORS:
+                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            elif token == '**':
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+    def calc(polish):
+        stack = []
+        for token in polish:
+            if token in OPERATORS:
+                y, x = stack.pop(), stack.pop()
+                if y == 0:
+                    if token == '/':
+                        print('0 cannot be divided')
+                        quit()
+                    else:
+                        stack.append(OPERATORS[token][1](x, y))
+            else:
+                stack.append(token)
+        return stack[0]
+
+    return calc(shunting_yard(parse(formula)))
+
+
+print(eval_(line))
---
+++ final_task/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pycalc',
+    version='1.0',
+    author='Molchanov Nikita',
+    author_email='molchanovnik7@gmail.com',
+    py_modules=['pycalc'],
+)
+
