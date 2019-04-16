221b3da3d42c8bb3573e27e8de8ff5c3bbb04145
Andrey Fesko <andrei.f@core-tech.ru>
2019-04-16 17:04:18 +0300

    60%

--- final_task/pycalc.py
+++ final_task/pycalc.py
@@ -1,5 +1,6 @@
-import math, unittest, re
-import tool, config
+import re
+import tool
+import config
 
 
 def validation(string):
@@ -8,8 +9,8 @@ def validation(string):
 
 def remove_space_between_operators(string):
     array = list(string)
-    for idx, chr in enumerate(array):
-        if chr == ' ':
+    for idx, token in enumerate(array):
+        if token == ' ':
             index = idx
             for char in array[idx+1:]:
                 array[index] = char
@@ -19,104 +20,111 @@ def remove_space_between_operators(string):
 
 
 def f_or_i(obj):
-    if type(obj) == tuple:
-        return obj
-    elif type(obj) == str:
+    if type(obj) == str:
         if obj[0] == '[':
             array = []
             for i in obj:
                 if i.isdigit():
                     array.append(f_or_i(i))
             return array
+        elif obj == 'True' or obj == 'False':
+            return bool(obj)
         elif '.' in obj:
-                return float(obj)
+            return float(obj)
         return int(obj)
     return obj
 
 
 def split_all_characters_and_numbers(string):
     array = []
-    array_brackets = 0
-    for idx, chr in enumerate(string):
-        if chr == ']':
-            array[-1] += chr
-            array_brackets = 0
-        elif array_brackets:
-            array[-1] += chr
-        elif chr == '[':
-            array.append(chr)
-            array_brackets = 1
-        elif chr in config.brackets or chr == ',':
-            array.append(chr)
-        elif chr in config.characters:
+    square_brackets = 0
+    for idx, token in enumerate(string):
+        if token == ']':
+            array[-1] += token
+            square_brackets = 0
+        elif square_brackets:
+            array[-1] += token
+        elif token == '[':
+            array.append(token)
+            square_brackets = 1
+        elif token in config.brackets or token == ',':
+            array.append(token)
+        elif token in config.characters:
             if len(array) == 0:
-                array.append(chr)
-            elif chr == '/' and array[-1] == '/':
-                    array[-1] += chr
-            elif chr == '-' or chr == '+':
+                array.append(token)
+            elif token == '/' and array[-1] == '/':
+                array[-1] += token
+            elif token == '-' or token == '+':
                 if array[-1] == '-' or array[-1] == '+':
                     if len(array) == 1:
-                        array[0] = -0
-                        array.append(chr)
+                        array[-1] += '1'
+                        array.append('*')
+                        array.append(token)
                     else:
-                        array.append(0)
-                        array.append(chr)
+                        if array[-2] == '(' or array[-2] in config.characters:
+                            array[-1] += '1'
+                            array.append('*')
+                            array.append(token)
+                        else:
+                            array.append('1')
+                            array.append('*')
+                            array.append(token)
                 else:
-                    array.append(chr)
+                    array.append(token)
             else:
-                array.append(chr)
-        elif chr == '.':
+                array.append(token)
+        elif token == '.':
             if len(array) == 0:
                 array.append('0.')
             elif array[-1][-1].isdigit():
-                array[-1] += chr
+                array[-1] += token
             elif array[-1] == '-':
                 array[-1] = '-0.'
             else:
                 array.append('0.')
-        elif chr.isdigit():
+        elif token.isdigit():
             if len(array) != 0:
                 if re.match(r'^log', array[-1]) or re.match(r'^expm', array[-1]):
-                    array[-1] += chr
+                    array[-1] += token
                     continue
             if len(array) == 0:
-                array.append(chr)
+                array.append(token)
             elif len(array) == 1:
                 if array[-1] == '-' or array[-1].isdigit() or array[-1][-1] == '.' or array[-1][-1].isdigit():
-                    array[-1] += chr
+                    array[-1] += token
                 elif array[-1] == '.':
-                    array[0] = '0.' + chr
+                    array[0] = '0.' + token
                 else:
-                    array.append(chr)
+                    array.append(token)
             else:
-                if array[-1] == '-':
+                if array[-1] == '-' or array[-1] == '+':
                     if array[-2] == '+' or array[-2] == '-':
-                        array[idx-1] = 0
-                        array.append('-' + chr)
+                        array[idx-1] = '0'
+                        array.append('-' + token)
                     elif array[-2] in config.characters or array[-2] == '(' or array[-2] == ',':
-                        array[-1] += chr
+                        array[-1] += token
                     else:
-                        array.append(chr)
+                        array.append(token)
                 elif array[-1].isdigit() or array[-1][-1].isdigit() or array[-1][-1] == '.':
-                    array[-1] += chr
+                    array[-1] += token
                 else:
-                    array.append(chr)
-        elif chr.isalpha():
+                    array.append(token)
+        elif token.isalpha():
             if len(array) == 0:
-                array.append(chr)
+                array.append(token)
             else:
                 if array[-1].isalpha() or re.match(r'^log1', array[-1]):
-                    array[-1] += chr
+                    array[-1] += token
                 else:
-                    array.append(chr)
+                    array.append(token)
     return array
 
 
 def constants_switch(array):
-    for chr in config.constants:
-        while chr in array:
-            idx = array.index(chr)
-            result = tool.constants_calculation(chr)
+    for token in config.constants:
+        while token in array:
+            idx = array.index(token)
+            result = tool.constants_calculation(token)
             array.pop(idx)
             array.insert(idx, result)
     return array
@@ -138,17 +146,17 @@ def brackets_calculation(array):
         brackets_inside = {}
         last = ''
         index = 0
-        for idx, chr in enumerate(array):
-            if chr == '(' and last == chr:
+        for idx, token in enumerate(array):
+            if token == '(' and last == token:
                 index = idx
-            elif chr == ')' and last == '':
+            elif token == ')' and last == '':
                 continue
-            elif chr == ')':
+            elif token == ')':
                 brackets_inside[index] = idx
                 last = ''
                 index = 0
-            elif chr == '(':
-                last = chr
+            elif token == '(':
+                last = token
                 index = idx
         offset = 0
         for key, value in brackets_inside.items():
@@ -158,12 +166,18 @@ def brackets_calculation(array):
                     return result
                 length = len(array[key-offset-1:value-offset+1])
                 first_index = key - offset - 1
-                for char in array[value +1 - offset:]:
+                for char in array[value+1-offset:]:
                     array[first_index] = char
                     first_index += 1
                 for i in range(length):
                     array.pop()
-                array.insert(key - offset -1, result)
+                if len(array) != 0:
+                    if array[key-offset-2] == '-' and (array[key-offset-3] == '(' or array[key-offset-3] in config.characters or array[key-offset-3] == ','):
+                        array[key-offset-2] += str(result)
+                    else:
+                        array.insert(key-offset-1, str(result))
+                else:
+                    array.append(result)
                 offset += length - 1
             else:
                 result = calculation(array[key+1-offset:value-offset])
@@ -178,14 +192,15 @@ def brackets_calculation(array):
                 offset += length - 1
     return array
 
+
 def calculation(array):
     if type(array) == tuple or type(array[0]) == list:
         return array
     if len(array) != 1:
-        for chr in config.characters:
-            while chr in array:
-                idx = array.index(chr)
-                result = tool.arithmetic(f_or_i(array[idx-1]), f_or_i(array[idx+1]), chr)
+        for token in config.characters:
+            while token in array:
+                idx = array.index(token)
+                result = tool.arithmetic(f_or_i(array[idx-1]), f_or_i(array[idx+1]), token)
                 if len(array) == 3:
                     return result
                 index = idx
@@ -199,7 +214,6 @@ def calculation(array):
         return array[0]
 
 
-@time_decorator
 def main(string):
     res = remove_space_between_operators(string)
     res = split_all_characters_and_numbers(res)
