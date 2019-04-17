7a77aa6762aa57a7a0d6098b844e47851a3792f6
novash-k <novash.ki@gmail.com>
2019-04-17 09:30:52 -0400

    branch novash setup main

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     url='https://github.com/novash-k/PythonHomework',  # Optional
     author='Konstantin Novash',  		# Optional
     author_email='novash.ki@gmail.com',  	# Optional
-    py_modules=["pycalc"]			# Required
-    entry-points={'pycalc': ['pycalc=pycalc:main']}
+    py_modules=["pycalc"],			# Required
+    entry_points = {'console_scripts': ['pycalc=pycalc:main',],},
     #packages=['pycalc_project']		# Required
     #python_requires='>3.0, <3.7'
     )
