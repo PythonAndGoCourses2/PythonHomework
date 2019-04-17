d90f7c1134b880d413247b82aa044939c99ad698
novash-k <novash.ki@gmail.com>
2019-04-17 19:35:03 -0400

    new func

--- final_task/pycalc.py
+++ final_task/pycalc.py
@@ -1,4 +1,4 @@
-#18.04.2019 00:20
+# 18.04.2019 00:20
 
 import argparse
 import math
@@ -9,34 +9,34 @@ ap.add_argument('EXPRESSION', type=str, help='expression string to evalute')
 ap.add_argument('-p', '--PRINT', default='n', choices=['y', 'n'], type=str, help='print evaluation process')
 ap.add_argument('-m', '--MODULE', type=str, help='use modules MODULE [MODULE...] additional modules to use')
 args=ap.parse_args()
-##print(args.EXPRESSION)
-##print(args.PRINT)
-##print(args.MODULE)
+# print(args.EXPRESSION)
+# print(args.PRINT)
+# print(args.MODULE)
 xpr=args.EXPRESSION
 show=args.PRINT
-#show='y'
+# show='y'
 
-##xpr=modstr=args.MODULE
-##xpr=
-##xpr=mod=__import__(modstr)
-##xpr=print (modstr, '=',mod.myfunc(3))
+# xpr=modstr=args.MODULE
+# xpr=
+# xpr=mod=__import__(modstr)
+# xpr=print (modstr, '=',mod.myfunc(3))
 
 
-## EVAL TEST
-##test=xpr
-##test=test.replace('^', '**')
-##test=test.replace(' ', '')
-##test=test.replace(', ', '.')
-##print ('EVAL:',test, '=',eval(test))
-##print(xpr)
+# EVAL TEST
+# test=xpr
+# test=test.replace('^', '**')
+# test=test.replace(' ', '')
+# test=test.replace(', ', '.')
+# print ('EVAL:',test, '=',eval(test))
+# print(xpr)
 
 oper=('!', '^', '//', '/', '*', '%', '-', '+', '(', ')', '==', '<=', '>=', '<', '>', '!=', '=')
 
-#func=('sin', 'cos', 'tan', 'log10', 'log', 'exp', 'abs', 'round', 'sqrt')
+# func=('sin', 'cos', 'tan', 'log10', 'log', 'exp', 'abs', 'round', 'sqrt')
 func=dir(math)
-print(func)
+#print(func)
 xprstr=''
-#word=''
+# word=''
 operator=''
 xprlst=[]
 a=0.
@@ -44,10 +44,10 @@ b=0.
 result=0.
 
 
-#разбор строки на элементы списка
+# разбор строки на элементы списка
 def parse(xprstr):
     word=''
-    #исправление неверно введенных знаков
+    # исправление неверно введенных знаков
     xprstr=xprstr.replace(' ', '')
     xprstr=xprstr.replace(', ', '.')
     xprstr=xprstr.replace('--', '+')
@@ -60,12 +60,12 @@ def parse(xprstr):
     xprstr=xprstr.replace('=>', '>=')
     xprstr=xprstr.replace('==+', '+')
     if xprstr[0] == '+': xprstr=xprstr[1:]
-    #print('parse:',xprstr)
+    # print('parse:',xprstr)
 
-    #разбор строки
-    for i,sym in enumerate(xprstr + ' '):     #добавлен дополнительный пробел
+    # разбор строки
+    for i,sym in enumerate(xprstr + ' '):     # добавлен дополнительный пробел
         if sym in oper or i == len(xprstr):
-            #print(i,word,sym)
+            # print(i,word,sym)
             if word == 'pi':
                 xprlst.append(pi)
             elif word == 'e':
@@ -79,12 +79,12 @@ def parse(xprstr):
                 exit(0)
             xprlst.append(sym)
             word=''
-            #print(sym)
+            # print(sym)
             
         else:
             word=word + sym
-        #print(i,sym,xprlst)
-    xprlst.pop()    #удаляется добавленный пробел      
+        # print(i,sym,xprlst)
+    xprlst.pop()    # удаляется добавленный пробел      
     
     for i,data in enumerate(xprlst):
         if xprlst[i] == '/' and xprlst[i + 1] == '/':
@@ -112,7 +112,7 @@ def parse(xprstr):
             xprlst[i - 1]='*'
             xprlst[i]=-1
             xprlst.insert(i + 1, '/')
-    #print(xprlst)        
+    # print(xprlst)        
     return(xprlst)
 
 
@@ -171,81 +171,81 @@ def operate(operator,a,b):
             print('Operate:',operator,a, '=',result)
     return result
 
-#вычисление выражения без скобок
+# вычисление выражения без скобок
 def calculate(xprlst):
     if show == 'y': print('Calculate:',xprlst)
     # перебор списка функций 
     for f in func:
         for i in range(xprlst.count(f)):
-           #print(f,xprlst.count(f))
+           # print(f,xprlst.count(f))
             s=xprlst.index(f)
             xprlst[s]=(operate(f,xprlst[s + 1],0))
             xprlst[s + 1]=''
             wipe(xprlst)
-            #print(*xprlst,sep='')
+            # print(*xprlst,sep='')
             
-    #вычисление возведение в степень с реверсом списка
-    #print('^ count:',xprlst.count('^'))
+    # вычисление возведение в степень с реверсом списка
+    # print('^ count:',xprlst.count('^'))
     if '^' in xprlst:
         xprlst.reverse()
-        #print('reverse: ',xprlst)
+        # print('reverse: ',xprlst)
         while '^' in xprlst:
             i=xprlst.index('^')
-            #print('i=',i)
+            # print('i=',i)
             xprlst[i]=xprlst[i + 1]**xprlst[i - 1]
-            #print(xprlst[i + 1], '^',xprlst[i - 1], '=',xprlst[i])
+            # print(xprlst[i + 1], '^',xprlst[i - 1], '=',xprlst[i])
             xprlst[i - 1]=''
             xprlst[i + 1]=''
-            #print(xprlst)
+            # print(xprlst)
             wipe(xprlst)
-            #print(xprlst)
+            # print(xprlst)
         xprlst.reverse()
         
-    #перебор списка математических операций
+    # перебор списка математических операций
     for j in oper:
-        #print('operation=',j)
-        #print(xprlst)
+        # print('operation=',j)
+        # print(xprlst)
         i=1
         while i < len(xprlst):
             if xprlst[i] == j:
-                #print('calculate: ',*xprlst,sep='')
+                # print('calculate: ',*xprlst,sep='')
                 xprlst[i]=operate(xprlst[i],xprlst[i - 1],xprlst[i + 1])
                 xprlst[i - 1]=''
                 xprlst[i + 1]=''
-                #print(xprlst)
+                # print(xprlst)
                 wipe(xprlst)
                 i=i - 1
             i=i + 1
-    #print('Stop calculate:',float(xprlst[0]))
+    # print('Stop calculate:',float(xprlst[0]))
     wipe(xprlst)
-    #print(xprlst)
+    # print(xprlst)
     result=xprlst[0]
     if len(xprlst) > 1:
         print('ERROR: missed operator')
-        #exit(0)
+        # exit(0)
     return(result)
 
-#очистка списка от пустых значений ''
+# очистка списка от пустых значений ''
 def wipe(xprlst):
-    #print('WIPE:\n',xprlst)
+    # print('WIPE:\n',xprlst)
     while '' in xprlst:
         i=xprlst.index('')
         xprlst.pop(i)
-    #print('WIPED:\n',xprlst)
+    # print('WIPED:\n',xprlst)
     return(xprlst)
 
-#поиск начала и конца выражения в скобках()
+# поиск начала и конца выражения в скобках()
 def brktindx(xprlst):
     bl=xprlst.index('(')
     br=xprlst.index(')')
     s=xprlst[bl + 1:br]
-    #print('BL BR ',bl + 1, ' ',br, ' ',*s,sep='')
+    # print('BL BR ',bl + 1, ' ',br, ' ',*s,sep='')
     while '(' in s:
         if s.count('(') == s.count(')'):
             bl=xprlst.index('(',bl + 1)
             br=xprlst.index(')',bl + 1)
             s=xprlst[bl + 1:br]
-            #print('BL BR ',bl + 1, ' ',br, ' ', *s,sep='')
+            # print('BL BR ',bl + 1, ' ',br, ' ', *s,sep='')
         else:
             br=xprlst.index(')',br + 1)
             s=xprlst[bl:br + 1]
@@ -254,33 +254,33 @@ def brktindx(xprlst):
 
 # основная функция
 def main(xpr):
-    #проверка скобок в строке
+    # проверка скобок в строке
     if xpr.count('(') != xpr.count(')'):
         print('ERROR: brackets are not balanced')
         exit(0)
 
-    #разбор строики в список
+    # разбор строики в список
     xprlst=parse(xpr)
-    #print(*xprlst,sep=', ')
+    # print(*xprlst,sep=', ')
 
 
 
 
 
-    #поиск скобок и вычисление в скобках
+    # поиск скобок и вычисление в скобках
     while '(' in xprlst:
         a,b=brktindx(xprlst)
-        #print('in brackets: ',*xprlst[a:b],sep='')
+        # print('in brackets: ',*xprlst[a:b],sep='')
         xprlst[a - 1]=calculate(xprlst[a:b])
         while a < b + 1:
             xprlst[a]=''
             a=a + 1
         wipe(xprlst)
-        #print(*xprlst,sep='')
+        # print(*xprlst,sep='')
 
-    #вычисление без скобок
+    # вычисление без скобок
     result=calculate(xprlst)
-    #print(result)
+    # print(result)
     return (result)
 
 print (main(xpr))
