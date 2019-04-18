24e3054ae9601439af9331b44ef2b94e98ee47f3
mishuk <mishuk.sk@live.ru>
2019-04-18 18:08:14 +0300

    style improvements

--- final_task/src/stack.py
+++ final_task/src/stack.py
@@ -1,13 +1,18 @@
 class Stack:
     def __init__(self, *args):
         self.items = [] if len(args) == 0 else args
+
     def isEmpty(self):
         return self.size() == 0
+
     def push(self, item):
         self.items.append(item)
+
     def pop(self):
         return self.items.pop()
+
     def size(self):
         return len(self.items)
+
     def lastItem(self):
         return self.items[len(self.items)-1]
