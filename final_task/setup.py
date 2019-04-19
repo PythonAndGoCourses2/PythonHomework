8f3ecfe5a2564af7347062c35a105b791f4ad2d4
Honnery <koksoholik@Gmail.com>
2019-04-19 22:26:26 +0300

    2345678

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
-setup(name='Final_TASK',
+from setuptools import setup, find_packages
+setup(name='FINAL_TASK',
 version='0.1',
 description='calculator',
 url='https://github.com/Honnery/PythonHomework/final_task',
 author='Honery',
 author_email='flyingcircus@example.com',
-packages=['pycalc'],
+packages=find_packages(),
 zip_safe=False,
 
 )
