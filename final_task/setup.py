8c7f6ec4cf9437655080c23ea927fe5a71c25965
Honnery <koksoholik@Gmail.com>
2019-04-19 21:32:20 +0300

    setuptools

---
+++ final_task/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name='pycalc',
+version='0.1',
+description='The funniest joke in the world',
+url='https://github.com/Honnery/PythonHomework/tree/master/final_task',
+author='Honery',
+author_email='flyingcircus@example.com',
+license='MIT',
+packages=['pycalc'],
+zip_safe=False,
+
+)
