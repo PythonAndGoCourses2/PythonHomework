d21d88391e51aad7fb7c50f3589d412899df696c
Vladislav <alienv661@gmail.com>
2019-04-22 22:22:10 +0300

    code refractor

--- final_task/pycalc/core/calculator.py
+++ final_task/pycalc/core/calculator.py
@@ -8,6 +8,40 @@ def get_priority(operand):
     return 1 if operand in low_priority else 2
 
 
+def handling_operator(polish_notation, num, stack, symbol):
+    '''if the lexeme is an operator, then:
+            if it is the last character of the expression, the expression is incorrect;
+            if this is a unary minus, then add it to the stack;
+            otherwise:
+                pushing the top elements of the stack into the result string, while the priority of the current operator is less than or equal to the priority of the operator located on the verine of the stack;
+                put the current operator in the stack;'''
+    polish_notation += num + ' '
+    while True:
+        if not stack:
+            break
+        if stack[-1] == '(' or (get_priority(symbol) > get_priority(stack[-1])):
+            break
+        elif get_priority(symbol) <= get_priority(stack[-1]):
+            polish_notation += stack.pop()
+    stack.append(symbol)
+
+    return polish_notation, ''
+
+
+def handling_closing_bracket(polish_notation, num, stack):
+    '''if the lexeme is a closing bracket, then:
+            we place elements from the stack into the resulting line until we meet the opening bracket, moreover, the opening bracket is removed from the stack, but not added to the resulting line;
+            if the function symbol is at the top of the stack, then we put it from the stack into the result string;
+            if the opening loop was not met, then the brackets are not consistent.'''
+    polish_notation += num + ' '
+    while True:
+        tmp = stack.pop()
+        if tmp == '(':
+            break
+        polish_notation += tmp + ' '
+
+    return polish_notation, ''
+
 def convert_into_rpn(expression):
     '''Rewrites the expression into the reverse polish notation'''
     arithmetic_operations = ('+', '-', '*', '/', '//', '%', '^')
@@ -17,31 +51,15 @@ def convert_into_rpn(expression):
     num = ''
     polish_notation = ''
 
-    # rewriting expression into reverse polish notation
     for symbol in expression:
         if symbol in arithmetic_operations:
-            polish_notation += num + ' '
-            num = ''
-            while True:
-                if not stack:
-                    break
-                if stack[-1] == '(' or (get_priority(symbol) > get_priority(stack[-1])):
-                    break
-                elif get_priority(symbol) <= get_priority(stack[-1]):
-                    polish_notation += stack.pop()
-            stack.append(symbol)
+            polish_notation, num = handling_operator(polish_notation, num, stack, symbol)
         elif symbol.isdigit() or symbol == '.':
             num += symbol
         elif symbol == '(':
             stack.append(symbol)
         elif symbol == ')':
-            polish_notation += num + ' '
-            num = ''
-            while True:
-                tmp = stack.pop()
-                if tmp == '(':
-                    break
-                polish_notation += tmp + ' '
+            polish_notation, num = handling_closing_bracket(polish_notation, num, stack)
     polish_notation += num + ' '
     while stack:
         polish_notation += stack.pop() + ' '
