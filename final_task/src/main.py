2f766268f765b31ffe3a6d11043f61840f86deb2
mishuk <mishuk.sk@live.ru>
2019-04-24 15:17:33 +0300

    started building external parsing

--- final_task/src/main.py
+++ final_task/src/main.py
@@ -1,4 +1,6 @@
 import argparse
+import sys
+from importlib import util
 from src import calculate
 
 
@@ -7,8 +9,19 @@ def calc():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "expression", help="Enter expression to evaluate", type=str)
+    parser.add_argument(
+        "-m",
+        "--module",
+        help="Used to provide external-defined functions. Write full or relative path to your module.",
+        type=str)
     args = parser.parse_args()
     try:
-        print(calculate.calculate(args.expression))
+        if args.module:
+            spec = util.spec_from_file_location("external", args.module)
+            module = util.module_from_spec(spec)
+            
+            print(calculate.calculate(args.expression, module))
+        else:
+            print(calculate.calculate(args.expression))
     except Exception as err:
         print("ERROR: {0}".format(err))
