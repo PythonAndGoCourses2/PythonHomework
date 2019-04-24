6a53f8eb2c7f602df5b9dbdc0115219148fb24a9
Denis Petrovsky <44451058+vertden@users.noreply.github.com>
2019-04-25 00:16:31 +0300

    Add setup.py

---
+++ final_task/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Calculate',
+    version='1.0',
+    packages=find_packages(),
+    description='Pure-python command-line calculator.',
+    author='Petrovsky Denis',
+    author_email='vertuss111@gmail.com',
+)
