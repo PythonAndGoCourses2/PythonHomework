d91559e0bb18037c9d210b0f68106b340a3d9d8b
Mad <den.gushcha@gmail.com>
2019-04-27 20:13:49 +0300

    try to write setup

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,1 +1,13 @@
-#test commit
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name = 'pycalc',
+    version = '0.1',
+    packages = [],
+    entry_points={ 
+        'console_scripts': [
+            'pycalc=pycalc:main',
+        ],
+    }
+)
