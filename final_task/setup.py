721d11667d73a448dd9114e0dd06d508df68dfdd
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-04-28 14:21:24 +0300

    fix: change setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
 
 setup(
-    name='pycalc',
-    version='1.0',
-    packages=['calc'],
+   name='pycalc',
+   version='1.0',
+   packages=find_packages(),
+   description='Pure-python command-line calculator.',
+   py_modules=['pycalc'],
+   entry_points={'console_scripts': ['pycalc = calc.pycalc:main', ], },
+   platforms='any',
 )
