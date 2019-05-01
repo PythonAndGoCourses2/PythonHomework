22649aad8aafa22556d36a3cfca1ba69082d6d48
Mad <den.gushcha@gmail.com>
2019-05-01 16:38:48 +0300

    fix setup problem

--- final_task/setup.py
+++ final_task/setup.py
@@ -11,7 +11,7 @@ setup(
     packages = find_packages(),
     entry_points={
         'console_scripts': [
-            'pycalc = pycalc.pycalc:main'
+            'pycalc = pycalc.__main__:main'
         ]
     }
 )
