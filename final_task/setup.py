a0406cef7391dc1191918b8b8b07511e22513dad
Denis Petrovsky <44451058+vertden@users.noreply.github.com>
2019-05-02 20:28:30 +0300

    Add files via upload

--- final_task/setup.py
+++ final_task/setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycalc',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     description='Pure-python command-line calculator.',
     author='Petrovsky Denis',
     author_email='vertuss111@gmail.com',
-    scripts=["pycalc.py"],
     py_modules=["pycalc", "parser"],
     entry_points={'console_scripts': ['pycalc=pycalc:main']}
 )
