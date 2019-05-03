c93d2edf014aeaec86d8d4630490dc55f593e6c3
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-03 15:08:55 +0300

    Fix tests for lexer

--- final_task/tests/lexer/test_lexer.py
+++ final_task/tests/lexer/test_lexer.py
@@ -9,41 +9,55 @@ class InitTestCase(unittest.TestCase):
         '''Test class __init__() method'''
 
         matchers = ['1']
-        lexer = Lexer(matchers)
+        source = 'some_source'
+        lexer = Lexer(matchers, source)
 
-        self.assertEqual(lexer._source, '')
+        self.assertEqual(lexer.source, source)
         self.assertEqual(lexer.matchers, matchers)
-        self.assertEqual(lexer._pos, 0)
-        self.assertEqual(lexer._length, 0)
+        self.assertEqual(lexer.pos, 0)
+        self.assertEqual(lexer.length, len(source))
 
-    def test_init(self):
+    def test_is_source_exhausted(self):
         """"""
 
-        matchers = ['1']
-        source = 'non_empty_string'
+        lexer = Lexer([], '')
+        test_cases = (
+            (0, 0, True),
+            (3, 3, True),
+            (1, 2, False),
+            (2, 1, True),
+        )
 
-        lexer = Lexer(matchers)
-        lexer._source = source * 2  # arbitrary number
-        lexer._pos += 13            # arbitrary number
-        lexer._length += 13         # arbitrary number
-
-        lexer.init(source)
-        self.assertEqual(lexer._source, source)
-        self.assertEqual(lexer._pos, 0)
-        self.assertEqual(lexer._length, len(source))
+        for pos, length, expected_result in test_cases:
+            with self.subTest(pos=pos,
+                              length=length,
+                              expected_result=expected_result
+                              ):
+                lexer = Lexer([], '')
+                lexer.pos = pos
+                lexer.length = length
+                self.assertEqual(lexer.is_source_exhausted(),
+                                 expected_result)
 
     @unittest.skip('not implemented')
-    def test_get_next_token(self):
+    def test_peek(self):
+        pass
+
+    @unittest.skip('not implemented')
+    def test_consume(self):
         pass
 
     @unittest.skip('not implemented')
     def test__next_token(self):
         pass
 
+    @unittest.skip('not implemented')
+    def test__match(self):
+        pass
+
     def test__skip_whitespaces(self):
         """"""
 
-        lexer = Lexer([])
         test_cases = (
             (' bcd', 0, 1),
             ('  cd', 0, 2),
@@ -57,10 +71,10 @@ class InitTestCase(unittest.TestCase):
             with self.subTest(source=source,
                               pos=pos,
                               expected_pos=expected_pos):
-                lexer.init(source)
-                lexer._pos = pos
+                lexer = Lexer([], source)
+                lexer.pos = pos
                 lexer._skip_whitespaces()
-                self.assertEqual(lexer._pos, expected_pos)
+                self.assertEqual(lexer.pos, expected_pos)
 
     def test__advance_pos_by_lexeme(self):
         """"""
@@ -69,33 +83,11 @@ class InitTestCase(unittest.TestCase):
         pos = 10
         expected_pos = pos + len(lexeme)
 
-        lexer = Lexer([])
-        lexer._pos = pos
+        lexer = Lexer([], '')
+        lexer.pos = pos
 
         lexer._advance_pos_by_lexeme(lexeme)
-        self.assertEqual(lexer._pos, expected_pos)
-
-    def test__check_source_end(self):
-        """"""
-
-        lexer = Lexer([])
-        test_cases = (
-            (0, 0, True),
-            (3, 3, True),
-            (1, 2, False),
-            (2, 1, True),
-        )
-
-        for pos, length, expected_result in test_cases:
-            with self.subTest(pos=pos,
-                              length=length,
-                              expected_result=expected_result
-                              ):
-                lexer = Lexer([])
-                lexer._pos = pos
-                lexer._length = length
-                self.assertEqual(lexer._is_source_exhausted(),
-                                 expected_result)
+        self.assertEqual(lexer.pos, expected_pos)
 
 
 if __name__ == '__main__':
