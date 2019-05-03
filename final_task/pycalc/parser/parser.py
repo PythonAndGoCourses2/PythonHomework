4c79d2049658b68ade34f43432829391ed33ce10
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-03 11:30:11 +0300

    Refactor a parser with consume, peek methods

--- final_task/pycalc/parser/parser.py
+++ final_task/pycalc/parser/parser.py
@@ -1,60 +1,87 @@
-""""""
-from pycalc.lexer.lexer import Lexer
-from pycalc.matcher.matcher import matchers
+"""
+Parser.
+"""
 
 
 class Parser:
     """"""
 
-    def __init__(self, lexer):
+    def __init__(self, registry, lexer):
+        self.registry = registry
         self.lexer = lexer
-        self.token = None
-        self.next_token = None
 
     def parse(self, source):
         """"""
 
         self.lexer.init(source)
-        self.next()
+        result = self.expression()
 
-        return self.expression()
+        assert self.lexer.source_exhausted(), \
+            f'Unparsed part of source left, (pos: {self.lexer._pos}).'
 
-    def expression(self, rbp=0):
+        return result
+
+    def expression(self, power=0):
         """"""
 
-        self.next()
-        left = self.token.prefix()
-        print('in expression start:', self.token, self.next_token, left)
-        while rbp < self.next_token.lbp:
-            self.next()
-            left = self.token.infix(left)
+        token = self.consume()
+        if not token:
+            raise Exception('i expect something but nothing finded')
+
+        left = token.nud()
+
+        while True:
+            token = self.peek()
+            if not token:
+                break
+
+            if power >= token.power.led:
+                break
+
+            self.consume()
+            left = token.led(left)
 
         return left
+
+    def consume(self):
+        """"""
+
+        return self.lexer.consume()
+
+    def peek(self):
+        """"""
+
+        return self.lexer.peek()
 
     def advance(self, token_class=None):
         """"""
 
-        if token_class and not self.next_token.is_instance(token_class):
+        token = self.peek()
+
+        if not token or (
+                token_class and
+                not token.is_instance(token_class)
+        ):
             raise SyntaxError(f"Expected: {token_class.__name__}")
 
-        self.next_token = self.lexer.get_next_token()
-
-    def next(self):
-        """"""
-
-        self.token = self.next_token
-        self.next_token = self.lexer.get_next_token()
+        self.consume()
 
 
 if __name__ == "__main__":
+    from pycalc.lexer.lexer import Lexer
+    from pycalc.matcher.matcher import matchers
+
     l = Lexer(matchers)
-    p = Parser(l)
-    assert p.parse('- - - 2 ** fn ( 1 , ( 2 + 1 ) * 5 , 4 )') == -1048576
+    p = Parser(None, l)
+    assert p.parse('2') == 2
+    assert p.parse('- 2') == 2
     assert p.parse('- - 2') == 2
+    assert p.parse('1 - - 2') == 3
     assert p.parse('4 ** 3 ** 2') == 262144
     assert p.parse('1 + 2 * 3') == 7
     assert p.parse('( 1 + 2 ) * 3') == 9
     assert p.parse('1 + 2 == 3') is True
     assert p.parse('0 == 1') is False
+    assert p.parse('- - - 2 ** fn ( 1 , ( 2 + 1 ) * 5 , 4 )') == -1048576
     # TODO:
     # assert p.parse('0 1') is False
