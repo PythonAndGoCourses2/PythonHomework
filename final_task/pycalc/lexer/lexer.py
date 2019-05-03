4a9b556d98f9243bad3ef5cdc96b8f017869e367
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-03 15:02:43 +0300

    Refactor lexer with comsumer/peek

--- final_task/pycalc/lexer/lexer.py
+++ final_task/pycalc/lexer/lexer.py
@@ -5,53 +5,64 @@ Lexer class.
 import re
 from collections import namedtuple
 
-from pycalc.matcher.matcher import matchers
-
 
 Token = namedtuple("Token", ("token_type", "lexeme"))
 WHITESPACES = re.compile(r"\s+")
 
 
 class Lexer:
-    """Represents"""
+    """Represents a lexer."""
 
-    def __init__(self, matchers):
+    def __init__(self, matchers, source):
         self.matchers = matchers
-        self._source = ""
-        self._pos = 0
-        self._length = 0
+        self.source = source
+        self.pos = 0
+        self.length = len(source)
 
-    def init(self, source):
-        """Init a lexer with a source string."""
+        # a wrapper for caching the last peeked token
+        self._token_wrapper = []
 
-        self._source = source
-        self._pos = 0
-        self._length = len(source)
+    def is_source_exhausted(self):
+        """Return `True` if the position pointer is out of the source string."""
 
-    def get_next_token(self):
-        """Return the next token.
+        assert(self.pos) >= 0
 
-        Raise exceptions when the source is exhausted or there are no matches."""
+        return self.pos >= self.length
 
-        self._skip_whitespaces()
+    def peek(self):
+        """"""
 
-        if self._is_source_exhausted():
-            raise Exception("EOL")
+        if self._token_wrapper:
+            return self._token_wrapper[-1]
 
         token = self._next_token()
+        self._token_wrapper.append(token)
 
-        if not token:
-            raise Exception("No match")
+        return token
 
-        self._advance_pos_by_lexeme(token.lexeme)
+    def consume(self):
+        """"""
+
+        token = self.peek()
+        self._token_wrapper.pop()
+        if token:
+            self._advance_pos_by_lexeme(token.lexeme)
 
         return token
 
     def _next_token(self):
+        """Try to match the next token."""
+
+        self._skip_whitespaces()
+        token = self._match()
+
+        return token
+
+    def _match(self):
         """Return `Token` or `None`."""
 
         for token_type, matcher in self.matchers:
-            lexeme = matcher(self._source, self._pos)
+            lexeme = matcher(self.source, self.pos)
 
             if not lexeme:
                 continue
@@ -64,7 +75,7 @@ class Lexer:
         """Skip whitespaces and advance the position index
         to the first non whitespace symbol."""
 
-        whitespaces = WHITESPACES.match(self._source, self._pos)
+        whitespaces = WHITESPACES.match(self.source, self.pos)
         if whitespaces:
             self._advance_pos_by_lexeme(whitespaces.group())
 
@@ -72,20 +83,17 @@ class Lexer:
         """Advance the position index by lexeme lenght."""
 
         value = len(lexeme)
-        self._pos += value
-
-    def _is_source_exhausted(self):
-        """Return `True` if the position pointer is out of the source string."""
-
-        assert(self._pos) >= 0
-
-        return self._pos >= self._length
+        self.pos += value
 
 
 if __name__ == "__main__":
+    from pycalc.matcher.matcher import matchers
 
     source = "  1.3 >=sin(pi   +e)  "
-    l = Lexer(matchers)
-    l.init(source)
+    l = Lexer(matchers, source)
     while True:
-        print(l.get_next_token())
+        token = l.consume()
+        if not token:
+            break
+        print(token, l.source, l.pos, l.length)
+
