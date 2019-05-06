19105f7a46aa1e1cb08ce4cba658cc728ba2f149
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-06 13:02:48 +0300

    Fix package docs

--- final_task/pycalc/importer/__init__.py
+++ final_task/pycalc/importer/__init__.py
@@ -1,6 +1,6 @@
 """
-Importer package provides functions for 
-modules importing and collecting module members.
+Importer package provides functions for
+modules importing and module members collecting.
 """
 
 from .importer import collect_members_by_type, import_modules
