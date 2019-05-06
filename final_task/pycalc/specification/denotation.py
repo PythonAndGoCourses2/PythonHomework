e201df6076cd9aea745a2fd65fac43832cc2ed4a
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-06 21:01:17 +0300

    Format code

--- final_task/pycalc/specification/denotation.py
+++ final_task/pycalc/specification/denotation.py
@@ -12,25 +12,20 @@ class Denotation:
         self.registry = {}
 
     def register(self, token_type, parselet, **kwargs):
-        """
-        Register token type with an appropriate parselet.
-        """
+        """Register token type with an appropriate parselet."""
+
         self._check_for_dup(token_type)
         self.registry[token_type] = parselet(**kwargs)
 
     def power(self, token):
-        """
-        Return power for a given token.
-        """
+        """Return power for a given token."""
 
         power = self._get_parselet(token).power
 
         return power
 
     def _get_parselet(self, token):
-        """
-        Find and return appropriate stored parselet for a given token type.
-        """
+        """Find and return appropriate stored parselet for a given token type."""
 
         token_type = token.token_type
 
