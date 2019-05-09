2812927d2c30cbf417dc98905772ec44d0900ac1
Honnery <koksoholik@Gmail.com>
2019-05-09 14:43:49 +0300

    add tests

---
+++ final_task/tests.py
@@ -0,0 +1,73 @@
+import unittest
+import math
+import pycalc.CheckAndChange as CheckAndChange
+import pycalc.difcalc as difcalc
+
+
+class TestFunctions(unittest.TestCase):
+
+    cheker = CheckAndChange.CheckAndChange()
+    calculator = difcalc.ComplexCalc()
+
+    def test_brackets(self):
+        self.assertEqual(self.cheker.correct_brackets("(()())"), None)
+        with self.assertRaises(Exception):
+            self.cheker.correct_brackets("(()()")
+            self.cheker.correct_brackets("())")
+            self.cheker.correct_brackets(")(")
+
+    def test_space_cheker(self):
+        self.assertEqual(self.cheker.correct_spaces("1*2"), None)
+        self.assertEqual(self.cheker.correct_spaces("1 * 2"), None)
+        with self.assertRaises(Exception):
+            self.cheker.correct_spaces("1 2*2")
+            self.cheker.correct_spaces("12 * * 2")
+
+    def test_numbers(self):
+        self.assertEqual(
+            self.cheker.do_all_changes(
+                "1122113", None), "1122113")
+        with self.assertRaises(Exception):
+            self.cheker.do_all_changes("", None)
+            self.cheker.do_all_changes(" ", None)
+
+    def test_module(self):
+        
+        with self.assertRaises(Exception):
+            self.cheker.addargs("module.py")
+
+    def test_comparison(self):
+        self.assertEqual(self.calculator.calculate(
+            "12+345+664+233445+2<2"), False)
+
+        with self.assertRaises(Exception):
+            self.calculator.calculate("12+345+664+233445+2<<<2")
+            self.calculator.calculate("12+345+664+233445+2=2")
+
+    def test_operators(self):
+        self.assertAlmostEqual(float(self.calculator.calculate("12^2")), 144.0)
+        self.assertAlmostEqual(float(self.calculator.calculate("12/2")), 6.0)
+        with self.assertRaises(Exception):
+            self.calculator.calculate("12^`2")
+            self.calculator.calculate("`12*2")
+            self.calculator.calculate("12/*2")
+
+    def test_funtions(self):
+        self.assertAlmostEqual(
+            float(
+                self.calculator.calculate("sin(4)")),
+            math.sin(4))
+        self.assertAlmostEqual(
+            float(
+                self.calculator.calculate("pow(2,1)")), math.pow(
+                2, 1))
+        with self.assertRaises(Exception):
+            self.calculator.calculate("sin()")
+            self.calculator.calculate("pow(1)")
+
+    def test_constants(self):
+        self.assertAlmostEqual(float(self.calculator.calculate("e")), math.e)
+
+
+if __name__ == '__main__':
+    unittest.main()
