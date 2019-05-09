e63cf227b9cf6ad61fb869ec0b4d80d606942ea7
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-09 13:39:03 +0300

    Add calculator's messages constants

---
+++ final_task/pycalc/calculator/messages.py
@@ -0,0 +1,8 @@
+"""
+Text constants for calculator messages.
+"""
+
+CALCULATOR_INITIALIZATION_ERROR = 'calculator initialization error'
+ERROR_MSG_PREFIX = 'ERROR: '
+EMPTY_EXPRESSION_PROVIDED = 'empty expression provided'
+SYNTAX_ERROR = 'syntax error'
