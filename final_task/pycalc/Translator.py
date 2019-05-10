89a5d7c2967882e43492ee4e177ae6be690e8234
Mad <den.gushcha@gmail.com>
2019-05-10 20:06:07 +0300

    Fix comparsions and power associativity

--- final_task/pycalc/Translator.py
+++ final_task/pycalc/Translator.py
@@ -1,8 +1,6 @@
 import sys
-from . import Tokens
+from . import Tokens as t
 
-
-# from Tokens import CONSTANTS, FUNCTIONS, func_delimiter, openBracket, closeBracket, OPERATORS
 
 def get_postfix(input_string):
     output_string = []
@@ -14,51 +12,54 @@ def get_postfix(input_string):
         if is_number(token):
             output_string.append(token)
             continue
-        if token in Tokens.CONSTANTS:
-            output_string.append(Tokens.CONSTANTS[token])
+        if token in t.CONSTANTS:
+            output_string.append(t.CONSTANTS[token])
             continue
-        if token in Tokens.FUNCTIONS:
+        if token in t.FUNCTIONS:
             stack.append(token)
             continue
-        if token == Tokens.func_delimiter:
-            while not stack[-1] == Tokens.openBracket:
+        if token == t.func_delimiter:
+            while not stack[-1] == t.openBracket:
                 output_string += stack.pop()
                 if not stack:
                     print('ERROR: miss delimiter or open bracket')
                     sys.exit(1)
             continue
-        if token in Tokens.OPERATORS:
+        if token in t.OPERATORS:
             if token == '-' or token == '+':
                 if (not is_number(input_string[index - 1])) or not index:
-                    if not input_string[index - 1] in Tokens.CONSTANTS:
+                    if not input_string[index - 1] in t.CONSTANTS:
                         output_string += '0'
-            while (stack[-1] in Tokens.OPERATORS) and (
-                    Tokens.OPERATORS[token].priority <= Tokens.OPERATORS[stack[-1]].priority):
+            while stack[-1] in t.OPERATORS and \
+                    ((token in t.left_associativity and t.OPERATORS[token].priority <= t.OPERATORS[
+                        stack[-1]].priority) or \
+                     (token in t.right_associativity and t.OPERATORS[token].priority < t.OPERATORS[
+                         stack[-1]].priority)):
                 output_string += stack.pop()
             else:
                 stack.append(token)
             continue
-        if token == Tokens.openBracket:
+        if token == t.openBracket:
             stack.append(token)
             continue
-        if token == Tokens.closeBracket:
-            while not stack[-1] == Tokens.openBracket:
+        if token == t.closeBracket:
+            while not stack[-1] == t.openBracket:
                 output_string += stack.pop()
                 if not stack:
                     print('ERROR: miss bracket')
                     sys.exit(1)
             stack.pop()
-            if stack[-1] in Tokens.FUNCTIONS:
+            if stack[-1] in t.FUNCTIONS:
                 output_string += [stack.pop()]
             continue
         print(f'ERROR: no such function or operator: \'{token}\'')
         sys.exit(1)
     while stack[-1]:
-        if stack[-1] == Tokens.openBracket:
+        if stack[-1] == t.openBracket:
             print('ERROR: expected close bracket')
             sys.exit(1)
-        if stack[-1] in Tokens.OPERATORS:
-            output_string += stack.pop()
+        if stack[-1] in t.OPERATORS:
+            output_string += [stack.pop()]
     return output_string
 
 
