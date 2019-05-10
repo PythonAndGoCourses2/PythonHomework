b66afc281fa934dbb3f61bcd9ed3052685e79253
Viacheslav <slavas.bu@gmail.com>
2019-05-10 18:17:01 +0300

    travis build fix

--- final_task/setup.py
+++ final_task/setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pycalc',
     version='1.0',
-    packages=find_packages(),
     description='Pure-python command-line calculator.',
-    py_modules=['pycalc', 'test'],
-    entry_points={'console_script': ['pycalc=pycalc:main']}
-    )
+    entry_points={'console_script': ['pycalc=pycalc:main']},
+    platforms='any',
+)
