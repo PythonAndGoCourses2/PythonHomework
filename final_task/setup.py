2fc80d9e8f055353ccecef5be6589ba37f5b42a6
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2019-05-10 19:44:58 +0300

    Refactoring

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
+from setuptools import setup, find_packages
 
-from setuptools import setup
 
-
-setup(name="pycalc",
-      version="1.0",
-      description="Pure-python command-line calculator",
-      url="https://github.com/WFLM/PythonHomework",
-      author="Yury Kuznetsov",
-      author_email="yurykuznetsov@protonmail.com",
-      scripts=["pycalc.py"],
-      entry_points={"console_scripts": ['pycalc=pycalc:main']}
-      )
+setup(
+    name="pycalc",
+    version="1.0",
+    author="Yury Kuznetsov",
+    author_email="yurykuznetsov@protonmail.com",
+    description="Pure-python command-line calculator",
+    url="https://github.com/WFLM/PythonHomework",
+    packages=find_packages(),
+    py_modules=["pycalc"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ['pycalc=pycalc.pycalc:main']}
+)
