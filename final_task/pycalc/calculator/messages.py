ea181fbe75ae3313a33a8c947a962d63590d7cc6
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-10 12:27:07 +0300

    Update messages constants

--- final_task/pycalc/calculator/messages.py
+++ final_task/pycalc/calculator/messages.py
@@ -3,6 +3,8 @@ Text constants for calculator messages.
 """
 
 CALCULATOR_INITIALIZATION_ERROR = 'calculator initialization error'
-ERROR_MSG_PREFIX = 'ERROR: '
+CANT_PARSE_EXPRESSION = 'can’t parse this expression'
 EMPTY_EXPRESSION_PROVIDED = 'empty expression provided'
+ERROR_MSG_PREFIX = 'ERROR: '
+MODULES_IMPORT_ERROR = 'no module(s) named'
 SYNTAX_ERROR = 'syntax error'
