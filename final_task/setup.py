bae7caf571e6f16d4a00eaa953039884c8b91aa5
Denis Petrovsky <44451058+vertden@users.noreply.github.com>
2019-05-11 17:57:39 +0300

    Add files via upload

--- final_task/setup.py
+++ final_task/setup.py
@@ -7,6 +7,6 @@ setup(
     description='Pure-python command-line calculator.',
     author='Petrovsky Denis',
     author_email='vertuss111@gmail.com',
-    py_modules=["pycalc", "parser","check",],
+    py_modules=["pycalc", "parser","check"],
     entry_points={'console_scripts': ['pycalc=pycalc:main']}
 )
