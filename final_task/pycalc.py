3b7b17471d255a4c5746917e950c34abedc61c27
Vaires <36207924+Vaires@users.noreply.github.com>
2019-05-12 23:31:06 +0300

    All test's fully passed.

--- final_task/pycalc.py
+++ final_task/pycalc.py
@@ -3,36 +3,69 @@ import re
 import math
 import argparse
 import operator
+from math import pi, e
 
 parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
 parser.add_argument('EXPRESSION', type=str, help='expression string to evaluate')
 expr = parser.parse_args().EXPRESSION
 
-MATH_FUNCTIONS = {name: (4, getattr(math, name)) for name in dir(math)}
+MATH_FUNCTIONS = {name: (5, "left", getattr(math, name)) for name in dir(math)}
+MATH_FUNCTIONS_NEG = {"-" + name: (5, "left", getattr(math, name)) for name in dir(math)}
 OPERATORS = {
-    '==': (1, operator.eq), '<': (1, operator.lt), '!=': (1, operator.ne),
-    '>': (1, operator.gt), '<=': (1, operator.le), '=>': (1, operator.ge),
-    '+': (2, operator.add), '^': (3, operator.pow), '-': (2, operator.sub),
-    '*': (3, operator.mul), '/': (3, operator.truediv), '//': (3, operator.floordiv),
-    '%': (3, operator.mod), 'abs': (4, operator.abs),
-    'round': (4, lambda x: round(x))
+    '==': (1, "left", operator.eq), '<': (1, "left", operator.lt), '!=': (1, "left", operator.ne),
+    '>': (1, "left", operator.gt), '<=': (1, "left", operator.le), '>=': (1, "left", operator.ge),
+    '+': (2, "left", operator.add), '^': (4, "right", operator.pow), '-': (2, "left", operator.sub),
+    '*': (3, "left", operator.mul), '/': (3, "left", operator.truediv), '//': (3, "left", operator.floordiv),
+    '%': (3, "left", operator.mod), 'abs': (5, "left", operator.abs), '-abs': (5, "left", operator.abs),
+    'round': (5, "left", lambda x: round(x)), '-round': (5, "left", lambda x: round(x))
 }
-NEW_DICTIONARY = dict(list(OPERATORS.items()) + list(MATH_FUNCTIONS.items()))
+NEW_DICTIONARY = dict(list(OPERATORS.items()) + list(MATH_FUNCTIONS.items()) + list(MATH_FUNCTIONS_NEG.items()))
+arg_count = 0
 
 
 def counting(expression):
+    def checklog(expression):
+        expression = re.sub(r'(?<=-)[+]+|[+]+(?=-)', "", expression)
+        match = re.search(r'^[-]{2,}', expression)
+        if match is not None:
+            check_expr = match.group(0)
+            len_pattern = len(check_expr)
+            if len_pattern % 2 == 0:
+                new_expression = re.sub(check_expr, "", expression)
+                checklog(new_expression)
+                return new_expression
+            elif len_pattern % 2 != 0:
+                new_expression = re.sub(check_expr, "-", expression)
+                checklog(new_expression)
+                return new_expression
+        else:
+            match = re.search(r'[-]{3,}', expression)
+            if match is not None:
+                check_expr = match.group(0)
+                len_pattern = len(check_expr)
+                if len_pattern % 2 == 0:
+                    new_expression = re.sub(check_expr, "--", expression)
+                    checklog(new_expression)
+                    return new_expression
+                elif len_pattern % 2 != 0:
+                    new_expression = re.sub(check_expr, "-", expression)
+                    checklog(new_expression)
+                    return new_expression
+            else:
+                return expression
 
     def check_entrence(check_expr, mistake=False):
+        check_expr = checklog(check_expr)
+        if check_expr is "":
+            return "ERROR: expression is empty."
         brackets_error = "ERROR: Brackets are not balanced."
-        cut_str = re.findall(r'[^()]', check_expr)
+        cut_str = re.findall(r'\(|\)', check_expr)
         if cut_str is not None:
             quantity = 0
-            for ind in cut_str:
-                use_expr = check_expr.replace(ind, '')
-            for ind in list(use_expr):
-                if ind == '(':
+            for index in cut_str:
+                if index == '(':
                     quantity = quantity + 1
-                elif ind == ')':
+                elif index == ')':
                     quantity = quantity - 1
                 if quantity < 0:
                     mistake = True
@@ -57,12 +90,12 @@ def counting(expression):
         if check:
             return space_between_operator_error
 
-        return calc(shunting_yard(parse(expression)))
+        return calc(shunting_yard(parse(check_expr)))
 
     def define_args(tokens_operator):
         global quantity_arg
         quantity_arg = 0
-        if tokens_operator.__class__ == 'function':
+        if tokens_operator.__name__ == '<lambda>':
             quantity_arg = tokens_operator.__code__.co_argcount
         else:
             match = re.search(r'\(.*\)', tokens_operator.__doc__)
@@ -75,31 +108,41 @@ def counting(expression):
         return quantity_arg
 
     def parse(expression_string):
-        number_re_neg = re.compile('^(-)?[0-9]+([.][0-9]+)?')
-        number_re_pos = re.compile('^[0-9]+([.][0-9]+)?')
-        list_for_reg_exp = [i for i in MATH_FUNCTIONS.keys()]
+        number_re_neg = re.compile('^((-)?[0-9]+([.][0-9]+)?)|(-)?[0-9]*([.][0-9]+)')
+        number_re_pos = re.compile('^([0-9]+([.][0-9]+)?)|([0-9]*([.][0-9]+))')
+        list_for_reg_exp = [index for index in MATH_FUNCTIONS.keys()]
         list_for_reg_exp.sort(key=len, reverse=True)
-        string_for_reg_exp = '{}'.format(' '.join('|({})'.format(i) for i in list_for_reg_exp))
-        operators_or_functions_to_compile = '^(==)|(>=)|(<=)|(!=)|(abs)|(round)|(//)|[><()^%+*-/]' + string_for_reg_exp
-        operators_or_functions_to_compile = operators_or_functions_to_compile.replace(' ', '')
-        operators_or_functions = re.compile(operators_or_functions_to_compile)
+        string_for_reg_exp = '{}'.format(' '.join('|({})'.format(index) for index in list_for_reg_exp))
+        string_for_reg_exp_with_neg = '{}'.format(' '.join('(-?{})|'.format(index) for index in list_for_reg_exp))
+        operators_or_functions_neg_to_compile = (
+                '^' + string_for_reg_exp_with_neg + '(==)|(>=)|(<=)|(!=)|(-?abs)|(-?round)|(//)|[><()^%+*-/]').replace(
+            ' ', '')
+        operators_or_functions_pos_to_compile = (
+                '^(==)|(>=)|(<=)|(!=)|(abs)|(round)|(//)|[><()^%+*-/]' + string_for_reg_exp).replace(' ', '')
+        operators_or_functions_pos = re.compile(operators_or_functions_pos_to_compile)
+        operators_or_functions_neg = re.compile(operators_or_functions_neg_to_compile)
         del_index = 0
-        expression_string.replace(' ', '')
-        for i in range(len(expression_string)):
+        expression_string = expression_string.replace(' ', '')
+
+        for index in range(len(expression_string)):
             if del_index:
                 del_index -= 1
                 continue
-            if expression_string[i - 1].isdigit() is False or i == 0:
-                number = number_re_neg.match(expression_string[i:])
+            if expression_string[index - 1].isdigit() is False or index == 0:
+                number = number_re_neg.match(expression_string[index:])
             else:
-                number = number_re_pos.match(expression_string[i:])
+                number = number_re_pos.match(expression_string[index:])
             if number:
                 end = number.end()
                 yield float(number.string[:end])
                 del_index = end - 1
                 continue
             else:
-                op = operators_or_functions.match(expression_string[i:])
+                if index == 0 or (
+                        expression_string[index - 1].isdigit() is False and expression_string[index - 1] != ")"):
+                    op = operators_or_functions_neg.match(expression_string[index:])
+                else:
+                    op = operators_or_functions_pos.match(expression_string[index:])
             if op:
                 end = op.end()
                 yield op.string[:end]
@@ -108,8 +151,20 @@ def counting(expression):
     def shunting_yard(parsed_expression):
         stack = []
         for token in parsed_expression:
-            if token in NEW_DICTIONARY:
-                while stack and stack[-1] != "(" and NEW_DICTIONARY[token][0] <= NEW_DICTIONARY[stack[-1]][0]:
+            if token in MATH_FUNCTIONS:
+                stack.append(token)
+                global arg_count
+                arg_count = 1
+            elif token is ',':
+                # out.append(stack.pop())
+                arg_count += 1
+                while stack[-1] != '(':
+                    y = stack.pop()
+                    yield y
+            elif token in NEW_DICTIONARY:
+                while stack and stack[-1] != "(" and ((NEW_DICTIONARY[token][1] is "left" and NEW_DICTIONARY[token][
+                    0] <= NEW_DICTIONARY[stack[-1]][0]) or (NEW_DICTIONARY[token][1] is "right" and
+                                                            NEW_DICTIONARY[token][0] < NEW_DICTIONARY[stack[-1]][0])):
                     yield stack.pop()
                 stack.append(token)
             elif token == ")":
@@ -126,21 +181,53 @@ def counting(expression):
             yield stack.pop()
 
     def calc(polish):
-        mistake_error = "ERROR: You have a mistake in your expression."
+        mistake_error = "ERROR:Invalid syntax"
         try:
             stack = []
             for token in polish:
                 if token in NEW_DICTIONARY:
-                    define_args(NEW_DICTIONARY[token][1])
-                    if quantity_arg == 1:
-                        x = stack.pop()
-                        stack.append(NEW_DICTIONARY[token][1](x))
-                    elif quantity_arg == 2:
+                    result = re.match(r'-?(pi|e|tau)', token)
+                    if result is not None:
+                        if result.group(0)[0] == "-":
+                            stack.append(- + NEW_DICTIONARY[token][2])
+                        else:
+                            stack.append(NEW_DICTIONARY[token][2])
+                    elif token == "log" or token == "-log":
+                        if arg_count == 2:
+                            y, x = stack.pop(), stack.pop()
+                            if token[0] == "-":
+                                stack.append(- + NEW_DICTIONARY[token][2](x, y))
+                            else:
+                                stack.append(NEW_DICTIONARY[token][2](x, y))
+                        else:
+                            x = stack.pop()
+                            if token[0] == "-":
+                                stack.append(- + NEW_DICTIONARY[token][2](x))
+                            else:
+                                stack.append(NEW_DICTIONARY[token][2](x))
+                    elif token == "-":
                         y, x = stack.pop(), stack.pop()
-                        stack.append(NEW_DICTIONARY[token][1](x, y))
+                        stack.append(NEW_DICTIONARY[token][2](x, y))
+                    else:
+                        define_args(NEW_DICTIONARY[token][2])
+                        if quantity_arg == 1:
+                            x = stack.pop()
+                            if token[0] == "-":
+                                stack.append(- + NEW_DICTIONARY[token][2](x))
+                            else:
+                                stack.append(NEW_DICTIONARY[token][2](x))
+                        elif quantity_arg == 2:
+                            y, x = stack.pop(), stack.pop()
+                            if token[0] == "-":
+                                stack.append(- + NEW_DICTIONARY[token][2](x, y))
+                            else:
+                                stack.append(NEW_DICTIONARY[token][2](x, y))
                 else:
                     stack.append(token)
-            return stack[0]
+            if len(stack) > 1:
+                return "ERROR:Invalid syntax"
+            else:
+                return stack[0]
         except IndexError:
             return mistake_error
 
