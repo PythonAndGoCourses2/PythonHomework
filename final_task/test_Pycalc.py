f0bdc37dc61fddcde3b0cb8b59e80bc3662ba627
Mad <den.gushcha@gmail.com>
2019-05-12 11:49:06 +0300

    Test translator module: is_unary and dell_spaces functions

--- final_task/test_Pycalc.py
+++ final_task/test_Pycalc.py
@@ -20,6 +20,16 @@ class TestTranslator(unittest.TestCase):
         self.assertEqual(['+', '19', '-', '2'], Translator.dell_spaces(['+', ' ', '19', ' ', '-', '2']))
         # self.assertRaises(Exeptions.InvalidStringError, Translator.dell_spaces, ['4', ' ', '5'])
 
+    def test_check_invalid_func(self):
+        pass
+
+    def test_is_unary(self):
+        self.assertFalse(Translator.is_unary(['2', '-', '1'], 1))
+        self.assertTrue(Translator.is_unary(['2', '+', '-', '1'], 2))
+
+    def test_check_unarys(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
