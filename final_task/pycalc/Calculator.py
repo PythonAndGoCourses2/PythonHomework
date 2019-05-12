92aedbd5812d76c44446245c15fdd97d4a30f887
Mad <den.gushcha@gmail.com>
2019-05-12 22:24:40 +0300

    Dell comments

--- final_task/pycalc/Calculator.py
+++ final_task/pycalc/Calculator.py
@@ -26,33 +26,3 @@ def calc(expr):
     if not len(stack) == 1:
         raise Exeptions.InvalidStringError()
     return stack.pop()
-    # try:
-    #     stack = []
-    #     for token in expr:
-    #         if token in Tokens.OPERATORS:
-    #             op2, op1 = stack.pop(), stack.pop()
-    #             stack.append(Tokens.OPERATORS[token].function(op1, op2))
-    #         elif token in Tokens.FUNCTIONS:
-    #             if token == 'pow' or token == 'log':
-    #                 stack_copy = stack[:]
-    #                 try:
-    #                     op2, op1 = stack_copy.pop(), stack_copy.pop()
-    #                     stack_copy.append(Tokens.FUNCTIONS[token](op1, op2))
-    #                     stack = stack_copy[:]
-    #                 except Exception:
-    #                     op = stack.pop()
-    #                     stack.append(Tokens.FUNCTIONS[token](op))
-    #             else:
-    #                 op = stack.pop()
-    #                 stack.append(Tokens.FUNCTIONS[token](op))
-    #         else:
-    #             stack.append(float(token))
-    #     if not len(stack) == 1:
-    #         raise Exeptions.InvalidStringError()
-    #     return stack.pop()
-    # except Exeptions.InvalidStringError:
-    #     print('ERROR: invalid string input')
-    #     exit(1)
-    # except Exception:
-    #     print('ERROR: something went wrong')
-    #     exit(1)
--- final_task/pycalc/Parser.py
+++ final_task/pycalc/Parser.py
@@ -10,11 +10,3 @@ def parse_console():
         return parser.parse_args().EXPRESSION
     else:
         raise Exeptions.GeneralError()
-    # try:
-    #     if parser.parse_args().EXPRESSION:
-    #         return parser.parse_args().EXPRESSION
-    #     else:
-    #         raise Exeptions.GeneralError()
-    # except Exeptions.GeneralError:
-    #     print('ERROR: empty input')
-    #     exit(1)
