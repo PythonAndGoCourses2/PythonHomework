d7abb7f0f860e656c3528c995545f97e2b25be68
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-05-13 07:41:19 +0300

    feat: update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='pycalc',
     version='1.0',
-    packages=["calculator"],
     description='Pure-python command-line calculator.',
+    long_description=read('README.md'),
+    packages=find_packages(),
+    python_requires='>=3.6',
     entry_points={
-        "console_scripts": ["pycalc=calculator.pycalc:main"]
+        "console_scripts": [
+            "pycalc=calculator.pycalc:main",
+        ]
     },
     platforms='any',
 )
