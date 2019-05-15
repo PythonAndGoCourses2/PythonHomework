8ac1b50e6a64882bd1fb953d34d760995fdc0b5d
Mikhail Sauchuk <mishasavchuk@gmail.com>
2019-05-16 00:50:11 +0300

    feat: extract metohds to parse numbers, functions, operators from spli_operators method, rename methods number_parser to number_chekc, operator_parser to operator_check, function_parser tp function_check

--- final_task/pycalc/expression_parser.py
+++ final_task/pycalc/expression_parser.py
@@ -21,18 +21,18 @@ class SplitOperators:
         self.last_symbol = ""
         self.blank_item = False
 
-    def operartor_parser(self, operator_symbol):
+    def operartor_check(self, operator_symbol):
         if operator_symbol in operator_dict.keys():
             return operator_symbol
         raise SyntaxError('Typo in math operator!')
 
-    def number_parser(self, number):
+    def number_check(self, number):
         try:
             return int(number)
         except ValueError:
             return float(number)
 
-    def function_parser(self, function_name):
+    def function_check(self, function_name):
         if function_name == 'e' or function_name == 'pi':
             return function_dict[function_name]['operator']
         elif function_name == 'tau':
@@ -49,14 +49,50 @@ class SplitOperators:
 
     def append_to_parsing_list(self):
         if self.last_symbol:
-            self.parsing_list.append(self.operartor_parser(self.last_symbol))
+            self.parsing_list.append(self.operartor_check(self.last_symbol))
             self.last_symbol = ""
         elif self.last_number:
-            self.parsing_list.append(self.number_parser(self.last_number))
+            self.parsing_list.append(self.number_check(self.last_number))
             self.last_number = ""
         elif self.last_letter:
-            self.parsing_list.append(self.function_parser(self.last_letter))
+            self.parsing_list.append(self.function_check(self.last_letter))
             self.last_letter = ""
+
+    def number_parser(self, number):
+        if self.blank_item and not isinstance(self.parsing_list[-1], str):
+            raise SyntaxError('Blank symbol between two operands!')
+        elif self.blank_item:
+            self.blank_item = False
+        if self.last_symbol:
+            self.append_to_parsing_list()
+        if self.last_letter:
+            self.last_letter += number
+        else:
+            if '.' in self.last_number and number == '.':
+                raise SyntaxError('Typo in the operand (two comma)!')
+            else:
+                self.last_number += number
+
+    def function_parser(self, letter):
+        if self.last_symbol:
+            self.append_to_parsing_list()
+        self.last_letter += letter
+
+    def twice_operator_parser(self, symbol):
+        if len(self.last_symbol) == 2:
+            raise SyntaxError('Invalid operator "{}"'.format(self.last_symbol + symbol))
+        if self.blank_item and str(self.parsing_list[-1]) in "!=<>/*":
+            raise SyntaxError('Blank symbol between twice operator')
+        elif self.blank_item:
+            self.blank_item = False
+        if not self.last_symbol:
+            self.append_to_parsing_list()
+        self.last_symbol += symbol
+
+    def simple_operator_bracket_parser(self, symbol):
+        self.append_to_parsing_list()
+        if symbol != ' ':
+            self.parsing_list.append(symbol)
 
     def split_operators(self, expression_line):
         if check_expression(expression_line):
@@ -65,37 +101,13 @@ class SplitOperators:
                     self.blank_item = True
                     self.append_to_parsing_list()
                 if i.isnumeric() or i == '.':
-                    if self.blank_item and not isinstance(self.parsing_list[-1], str):
-                        raise SyntaxError('Blank symbol between two operands!')
-                    elif self.blank_item:
-                        self.blank_item = False
-                    if self.last_symbol:
-                        self.append_to_parsing_list()
-                    if self.last_letter:
-                        self.last_letter += i
-                    else:
-                        if '.' in self.last_number and i == '.':
-                            raise SyntaxError('Typo in the operand (two comma)!')
-                        else:
-                            self.last_number += i
+                    self.number_parser(i)
                 elif i.isalpha():
-                    if self.last_symbol:
-                        self.append_to_parsing_list()
-                    self.last_letter += i
+                    self.function_parser(i)
                 elif i in "!=<>/*":
-                    if len(self.last_symbol) == 2:
-                        raise SyntaxError('Invalid operator "{}"'.format(self.last_symbol + i))
-                    if self.blank_item and str(self.parsing_list[-1]) in "!=<>/*":
-                        raise SyntaxError('Blank symbol between twice operator')
-                    elif self.blank_item:
-                        self.blank_item = False
-                    if not self.last_symbol:
-                        self.append_to_parsing_list()
-                    self.last_symbol += i
+                    self.twice_operator_parser(i)
                 else:
-                    self.append_to_parsing_list()
-                    if i != ' ':
-                        self.parsing_list.append(i)
+                    self.symple_operator_bracket_parser(i)
             if self.last_symbol:
                 raise SyntaxError(
                     'Extra operator "{}" at the end of the expression!'.format(self.last_symbol)
