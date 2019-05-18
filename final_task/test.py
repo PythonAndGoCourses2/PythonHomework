7e78d92c3d1e3c09e6ffc0f03bb56cb02e903fae
Denis Petrovsky <44451058+vertden@users.noreply.github.com>
2019-05-18 13:55:07 +0300

    full support for function

--- final_task/test.py
+++ final_task/test.py
@@ -5,9 +5,7 @@ from math import *
 
 class PycalcTest(unittest.TestCase):
     def test_check(self):
-        self.assertEqual(pycalc.calculating("pi+e"), pi + e)
-        self.assertEqual(pycalc.calculating("(2.0^(pi/pi+e/e+2.0^0.0))"), (2.0 ** (pi / pi + e / e + 2.0 ** 0.0)))
-        self.assertEqual(pycalc.calculating("(2.0^(pi/pi+e/e+2.0^0.0))"), (2.0 ** (pi / pi + e / e + 2.0 ** 0.0)))
+        self.assertEqual(pycalc.calculating("2+2"), 2+2)
 
 
 if __name__ == '__main__':
