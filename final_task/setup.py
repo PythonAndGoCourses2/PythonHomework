74f52fdf19fbd262026087e5afa9035bd158c606
Stashchen <45607324+Stashchen@users.noreply.github.com>
2019-05-20 21:48:18 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="stashchen@gmail.com",
     description="Pure-python command line calculator",
     entry_points={
-        "console_scripts": ["pycalc=Calculator.calc:main"]},
+        "console_scripts": ["pycalc=final_task.calc:main"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
