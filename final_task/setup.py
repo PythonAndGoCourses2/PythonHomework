a2b779ad3e6df4c1f1c110243e615a346e060d63
Pavel Kitaiharodski <htonichen@gmail.com>
2019-05-21 18:52:06 +0300

    trying to run travis

--- final_task/setup.py
+++ final_task/setup.py
@@ -2,8 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="pycalc",
     packages=find_packages(),
-    version = "1.0.0",
-    entry_points = {
-        'console_scripts':['pycalc = src.pycalc:main']
-    }
+    version = "1.0.0"
 )
