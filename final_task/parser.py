6477ad228e50f9bde53ff05855e338fd4d280bf2
Denis Petrovsky <44451058+vertden@users.noreply.github.com>
2019-05-23 12:40:41 +0300

    Delete parser.py

--- final_task/parser.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-def create_parser():
-    parser = argparse.ArgumentParser(prog='pycalc', description="Pure-python command-line calculator.")
-    parser.add_argument('EXPRESSION', help='expression string to evaluate', type=str)
-    return parser.parse_args()
-
-# Создать оформление справки и ошибок
