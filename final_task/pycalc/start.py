6e6b4754455ca4d27c9bed77db2cbd78ad635f91
Andrei Trukhan <44117784+BadMadNobody@users.noreply.github.com>
2019-05-23 11:16:02 +0300

    Add files via upload

--- final_task/pycalc/start.py
+++ final_task/pycalc/start.py
@@ -7,13 +7,13 @@ def main():
     parser.add_argument("EXPRESSION", type=str, help='expression string to evaluate')
     parser.add_argument('-m MODULE[MODULE...]', '--use-modules MODULE[MODULE...]', nargs='*',
                         help='additional modules to use', dest='modules', type=str, default=['math'])
-    args = parser.parse_args(["1+2*3=="])
-
+    args = parser.parse_args()
     answer = Main.pycalc(args.EXPRESSION, args.modules)
     if type(answer) == float or type(answer) == int:
         if answer % 1 == 0:
-            return int(answer)
+            print(int(answer))
         else:
-            return answer
+            print(answer)
     else:
-        return answer
+        print(answer)
+
