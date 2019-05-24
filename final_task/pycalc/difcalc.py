db936995d63b2a035a71b8d05fade14dd927b7f5
Honnery <koksoholik@Gmail.com>
2019-05-24 18:22:18 +0300

    Revert "Revert "`1`2`""
    This reverts commit c49ae3e3cfbb25eb22432129db50783b59dd09db.

--- final_task/pycalc/difcalc.py
+++ final_task/pycalc/difcalc.py
@@ -59,7 +59,7 @@ class ComplexCalc(easyCalculation.Calculator):
                     end = place + len(rezult) - 1
                     expr = self._calc_if_power(expr, place, end)
 
-    def _find_replacement(self, func:str, expr:str) -> str:
+    def _find_replacement(self, func, expr):
 
         if func in ComplexCalc.math_functions:
             allargs = self._commasplit(expr)
