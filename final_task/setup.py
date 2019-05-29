d13aa1d5d7b7f61166a105ed18d2087937dfef34
p4elopuh <51139908+p4elopuh@users.noreply.github.com>
2019-05-29 12:37:41 +0300

    Update setup.py

---
+++ final_task/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pycalc",
+    version="0.1",
+    author="Tatsiana Kavalchuk",
+    author_email="p4elopuh@gmail.com",
+    description="Pure-python command-line calculator",
+    #url="https://github.com//PythonAndGoCourses2/PythonHomework",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
