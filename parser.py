37114ecfff8bd4d3deebcf9f55158c6d95f97a73
Alexander Sereda <alsereda1992@gmail.com>
2019-05-30 21:17:05 +0300

    right parser

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ OPERATORS = ('+', '-', '*', '/', '//', '%', '^', '<', '<=', '==', '!=', '>=', '>
 
 
 def get_token(input_expression):
+    """Getting a tokens from input string"""
     if input_expression.count('(') != input_expression.count(')'):
         print('ERROR: the number of opening and closing brackets must match')
         sys.exit(1)
@@ -19,7 +20,11 @@ def get_token(input_expression):
                 raw_tokens[-1] = raw_tokens[-1] + i
             elif i.isdigit() and '.' in raw_tokens[-1]:
                 raw_tokens[-1] = raw_tokens[-1] + i
-            elif i.isalpha() and raw_tokens[-1].isalpha():
+            elif i.isalpha() and raw_tokens[-1].isalnum():
+                raw_tokens[-1] = raw_tokens[-1] + i
+            elif i.isdigit() and raw_tokens[-1].isalpha():
+                raw_tokens[-1] = raw_tokens[-1] + i
+            elif i.isdigit() and raw_tokens[-1].isalnum():
                 raw_tokens[-1] = raw_tokens[-1] + i
             elif i == '/' and raw_tokens[-1] == '/':
                 raw_tokens[-1] = raw_tokens[-1] + i
@@ -31,29 +36,32 @@ def get_token(input_expression):
 
 
 def separate_function(raw_tokens):
+    """Selecting function arguments"""
     tokens = ['']
-    brackets_counter = 0
+    stack = []  # To define a closing bracket or end of function
     while raw_tokens:
         token = raw_tokens[0]
         raw_tokens = raw_tokens[1:]
-        if token == '(' and tokens[-1].isalpha():
-            brackets_counter += 1
-            tokens.append('[')
-        elif token == '(':
-            brackets_counter += 1
-            tokens.append(token)
-        elif token == ')' and '[' in tokens:
-            brackets_counter -= 1
-            if brackets_counter:
-                tokens.append(token)
+        if token == '(':
+            if tokens[-1].isalnum():  # Selection start of function
+                tokens.append('[')
+                stack.append('[')
             else:
+                tokens.append(token)
+                stack.append(token)
+        elif token == ')':
+            x = stack.pop() 
+            if x == '[':  # Define a closing bracket or end of function
                 tokens.append(']')
+            else:
+                tokens.append(token)
         else:
             tokens.append(token)
     return tokens[1:]
 
 
 def create_infix(tokens):
+    """Adding unary operations, constants and converting strings to numbers"""
     infix = ['']
     while tokens:
         token = tokens[0]
@@ -72,4 +80,5 @@ def create_infix(tokens):
 
 
 def parse_input_expression(input_string):
+    """Issuing tokens"""
     return create_infix(separate_function(get_token(input_string)))
--- pycalc.py
+++ pycalc.py
@@ -3,7 +3,7 @@ import parser
 from sorter import create_polish_notation
 from calculation import calc
 
-s = 'sin(-cos(-sin(3.0)-cos(-sin(-3.0*5.0)-sin(cos(log10(43.0))))+cos(sin(sin(34.0-2.0^2.0))))--cos(1.0)--cos(0.0)^3.0)'
+s = 'sin(1/(5-sin(cos(3*(7-atan2(4)-8))/9)))'
 print(s)
 t = parser.parse_input_expression(s)
 print(t)
