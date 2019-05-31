2a7d2e8771cc6288b92e4f441d9985b5ae69d6e8
Alexander Sereda <alsereda1992@gmail.com>
2019-05-31 23:58:55 +0300

    one stap back

--- final_task/pycalc/parser.py
+++ final_task/pycalc/parser.py
@@ -75,8 +75,6 @@ def create_infix(tokens):
         elif token == '+' and (infix[-1] == '' or infix[-1] in constants.OPERATORS or infix[-1] == '('):
             infix.append('pos')
         elif token.isnumeric():
-            if type(infix[-1]) is float:
-                raise Exception('ERROR: Something went wrong')
             infix.append(float(token))
         else:
             infix.append(token)
