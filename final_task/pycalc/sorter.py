83de84b490326db9e94f72acaca414273ec2efc8
Alexander Sereda <alsereda1992@gmail.com>
2019-06-01 01:39:40 +0300

    fix sorter

--- final_task/pycalc/sorter.py
+++ final_task/pycalc/sorter.py
@@ -9,12 +9,12 @@ def create_polish_notation(parsed_formula):
     while parsed_formula:
         token = parsed_formula[0]
         parsed_formula = parsed_formula[1:]
-        if token in constants.OPERATORS or token in constants.FUNCTIONS:
+        if token in constants.OPERATORS:
             while stack_operation and stack_operation[-1] != '(' and \
                     ((token in constants.LEFT_ASSOCIATIVITY_OPERATORS and constants.PRIORITY[token] <=
                         constants.PRIORITY[stack_operation[-1]]) or
                         (token in constants.RIGHT_ASSOCIATIVITY_OPERATORS and constants.PRIORITY[token] <
-                            constants.PRIORITY[stack_operation[-1]])) and token in constants.FUNCTIONS_PRIORITY:
+                            constants.PRIORITY[stack_operation[-1]])) and not stack_function:
                 polish_notation.append(stack_operation.pop())
             stack_operation.append(token)
         elif token == ')':
@@ -30,10 +30,9 @@ def create_polish_notation(parsed_formula):
         elif token == '[':
             continue
         elif token == ']':
-            continue
-            #while stack_operation:
-                #polish_notation.append(stack_operation.pop())
-            #polish_notation.append(stack_function.pop())
+            while stack_operation:
+                polish_notation.append(stack_operation.pop())
+            polish_notation.append(stack_function.pop())
         else:
             polish_notation.append(token)
     while stack_operation:
