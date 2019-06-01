20c51fdffc8162f3d4da48970d1d577948e4e7ed
Alexander Sereda <alsereda1992@gmail.com>
2019-06-01 15:56:53 +0300

    add some check

--- final_task/pycalc/parser.py
+++ final_task/pycalc/parser.py
@@ -94,7 +94,7 @@ def check(infix):
     while infix:
         stack.append(infix[0])
         infix = infix[1:]
-        if type(stack[-1]) == float and type(infix[0]):
+        if type(stack[-1]) == float and type(infix[0]) == float:
             raise Exception('ERROR: Something went wrong')
     return stack
 
