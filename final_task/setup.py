133d596b9cf826fdcb8e26ff538946fb346623dc
Durnevir <Durnevir@users.noreply.github.com>
2019-06-02 23:58:14 +0300

    Add files via upload

---
+++ final_task/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name="pycalc.py",
+    version="0.1",
+    url="https://github.com/Durnevir/PythonHomework",
+    packages=find_packages(),
+)
