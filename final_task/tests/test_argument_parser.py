331c0f26aeebdf5656ceb0d613eeebfcd454ca8d
Mikhail Sauchuk <mishasavchuk@gmail.com>
2019-06-05 01:06:23 +0300

    feat: add unittest for arg_parser with user modules

--- final_task/tests/test_argument_parser.py
+++ final_task/tests/test_argument_parser.py
@@ -1,8 +1,10 @@
 import unittest
-from unittest import mock
+from unittest.mock import patch
 import argparse
+import math
 from pycalc.argument_parser import arg_parser
 from pycalc.operator_manager import create_func_dict
+from pycalc.use_module_test import sin, user_function, CONSTANT, pi
 
 
 class TestArgumentParser(unittest.TestCase):
@@ -10,8 +12,23 @@ class TestArgumentParser(unittest.TestCase):
     def setUp(self):
         self.func_dict = create_func_dict()
 
-    @mock.patch('argparse.ArgumentParser.parse_args',
+    @patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace(EXPRESSION='2+40', use_modules=""))
     def test_expression_parser(self, mock_args):
         test_line = arg_parser()
         self.assertEqual(('2+40', self.func_dict), test_line)
+
+    @patch('argparse.ArgumentParser.parse_args',
+                return_value=argparse.Namespace(EXPRESSION='2+40', use_modules="use_module_test"))
+    def test_expression_and_module_parser(self, mock_args):
+        user_functions = {
+                        'sin': sin,
+                        'user_function': user_function,
+                        'CONSTANT': CONSTANT,
+                        'pi': pi
+                        }
+        func_dict = create_func_dict(user_functions)
+        test_line = arg_parser()
+        self.assertEqual(('2+40', func_dict), test_line)
+        self.assertNotEqual(math.sin, test_line.functions['sin']['operator'])
+        self.assertNotEqual(math.pi, test_line.functions['pi'])
