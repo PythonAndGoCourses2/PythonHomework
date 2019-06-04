da41b6e5d9760a92d005e3ba95dce3d173768b70
Mikhail Sauchuk <mishasavchuk@gmail.com>
2019-06-05 02:21:47 +0300

    fix: add ..mod to the path of user_module

--- final_task/pycalc/operator_manager.py
+++ final_task/pycalc/operator_manager.py
@@ -33,8 +33,7 @@ def find_user_functions(module):
     :return: dict {function_name: function}
     """
     try:
-        module_path = os.path.commonprefix([module])
-        user_module = importlib.import_module(module_path)
+        user_module = importlib.import_module('..{}'.format(module))
         item = [i for i in dir(user_module) if not i.startswith('_')]
         user_functions = {i: user_module.__dict__[i] for i in item}
         return user_functions
