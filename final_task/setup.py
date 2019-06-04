08ddfabdd91e9415ea51e202b1a6961050b51d64
Antossh <51364187+Antossh@users.noreply.github.com>
2019-06-04 23:19:18 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -6,7 +6,5 @@ setup(
     author='Antos Shakhbazau',
     author_email='Shakhbazau@gmail.com',
     packages=find_packages(),
-    entry_points={'console_scripts': ['pycalc = pycalc.cli:main']},
     description='Pure-python command line calculator.',
-    py_modules=['pycalc']
-)
+    )
