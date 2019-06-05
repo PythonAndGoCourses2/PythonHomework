9231382c236c31fe5c868a3dfeba7e9079371a2e
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-06-05 11:14:39 +0300

    Raise exceptions on calculator errors

--- final_task/pycalc/calculator/calculator.py
+++ final_task/pycalc/calculator/calculator.py
@@ -4,12 +4,14 @@ Initialization of a calculator. Return a calculator instance.
 
 from pycalc.lexer import Lexer
 from pycalc.parser import Parser, ParserGenericError
+from pycalc.importer import ModuleImportErrors
 
-from .formatters import err_msg_formatter, err_ctx_formatter
-from .errors import get_err_msg
+from .formatters import err_msg_with_ctx_formatter, err_modules_import_formatter
+from .errors import CalculatorCalculationError, CalculatorInitializationError, get_err_msg
 from .importer import build_modules_registry
 from .matchers import build_matchers
 from .messages import (
+    CALCULATOR_INITIALIZATION_ERROR,
     CANT_PARSE_EXPRESSION,
     EMPTY_EXPRESSION_PROVIDED,
 )
@@ -31,13 +33,13 @@ class Calculator:
         """
         Calculate an expression.
 
-        Return result of a calculation or an error message
-        if the calculation fails.
+        Return result of calculation or raise a `CalculatorCalculationError` exception
+        if calculation fails.
         """
 
         # empty expression
         if not expression:
-            return err_msg_formatter(EMPTY_EXPRESSION_PROVIDED)
+            raise CalculatorCalculationError(EMPTY_EXPRESSION_PROVIDED)
 
         # calculate an expression
         try:
@@ -53,41 +55,48 @@ class Calculator:
 
             # an error message
             err_msg = get_err_msg(exc)
-            err_msg = err_msg_formatter(err_msg)
+            err_msg = err_msg_with_ctx_formatter(err_msg, ctx)
 
-            # an context message
-            ctx_msg = err_ctx_formatter(ctx)
-
-            return f'{err_msg}\n{ctx_msg}'
-
-        # probably not reacheable code but better save than sorry
+        # handle all other errors
         except Exception as exc:
+            err_msg = CANT_PARSE_EXPRESSION
 
-            err_msg = err_msg_formatter(CANT_PARSE_EXPRESSION)
-
-            return err_msg
+        raise CalculatorCalculationError(err_msg)
 
 
 def calculator(modules_names=None):
-    """Initialize a calculator and return a parser object."""
+    """
+    Initialize a calculator and return a Calculator instance.
 
-    # import constants and functions from default and requested modules
-    modules_registry = build_modules_registry(modules_names)
+    Raise a `CalculatorInitializationError` exception when initialization fails."""
 
-    # build lexemes matchers
-    matchers = build_matchers(modules_registry)
+    try:
+        # import constants and functions from default and requested modules
+        modules_registry = build_modules_registry(modules_names)
 
-    # create a lexer
-    lexer = Lexer(matchers)
+        # build lexemes matchers
+        matchers = build_matchers(modules_registry)
 
-    # build a specification for a parser
-    spec = build_specification(modules_registry)
+        # create a lexer
+        lexer = Lexer(matchers)
 
-    # create a parser
-    power = Precedence.DEFAULT
-    parser = Parser(spec, lexer, power)
+        # build a specification for a parser
+        spec = build_specification(modules_registry)
 
-    # create a calculator
-    calculator_ = Calculator(parser)
+        # create a parser
+        power = Precedence.DEFAULT
+        parser = Parser(spec, lexer, power)
 
-    return calculator_
+        # create a calculator
+        calculator_ = Calculator(parser)
+
+        return calculator_
+
+    except ModuleImportErrors as exc:
+        modules_names = exc.modules_names
+        err_msg = err_modules_import_formatter(modules_names)
+
+    except Exception:
+        err_msg = CALCULATOR_INITIALIZATION_ERROR
+
+    raise CalculatorInitializationError(err_msg)
