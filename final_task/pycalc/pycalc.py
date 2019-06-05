41cc59630a06741d6a7ff00d1f553d63f7b9192f
Alexander Sereda <alsereda1992@gmail.com>
2019-06-05 23:53:43 +0300

    try to add additional task

--- final_task/pycalc/pycalc.py
+++ final_task/pycalc/pycalc.py
@@ -1,22 +1,25 @@
 import argparse
+import sys
 from .parser import parse_input_string
 from .sorter import create_polish_notation
 from .calculation import get_result
-
-
-def create_parser():
-    parser = argparse.ArgumentParser(description="Pure-python command-line calculator.")
-    parser.add_argument('EXPRESSION', help='Input string to evaluate',)
-    args = parser.parse_args()
-    input_string = args.EXPRESSION
-    return input_string
+from . import constants
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Pure-python command-line calculator.")
+    parser.add_argument('EXPRESSION', help='Input string to evaluate', type=str)
+    parser.add_argument('-m', '--modules', help='Additional modules to use', type=str)
+    args = parser.parse_args()
     try:
-        input_string = create_parser()
-        if input_string:
-            tokens_in_infix = parse_input_string(input_string)
+        if args.EXPRESSION:
+            if args.modules:
+                for item in args.modules:
+                    __import__(item)
+                    add_functions = dict([(attr, getattr(sys.modules[item], attr)) for attr in dir(sys.modules[item]) if
+                                          callable(getattr(sys.modules[item], attr))])
+                    constants.FUNCTIONS = {**constants.FUNCTIONS, **add_functions}
+            tokens_in_infix = parse_input_string(args.EXPRESSION)
             tokens_in_polish = create_polish_notation(tokens_in_infix)
             print(get_result(tokens_in_polish))
         else:
