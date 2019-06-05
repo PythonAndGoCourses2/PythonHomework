49884a212bba58248681c5e53b2f666d3ee51082
alinonax <alinonax@gmail.com>
2019-06-05 23:12:58 +0400

    Fix unittests_validation 2

--- final_task/unittests/unittests_validation.py
+++ final_task/unittests/unittests_validation.py
@@ -23,8 +23,7 @@ class ExpectedFailureTestCase(unittest.TestCase):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['==', 7.0]))
 
     def test6(self):
-        self.assertEqual(True,
-                          lambda: calc_obj.validate_parsed_list([1.0, '+', 2.0, '(', 3.0, '*', 4.0, ')', ')']))
+        self.assertEqual(True, lambda: calc_obj.validate_parsed_list([1.0, '+', 2.0, '(', 3.0, '*', 4.0, ')', ')']))
 
     def test7(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '(', 1.0, '+', 2.0, ')']))
@@ -66,8 +65,8 @@ class ExpectedFailureTestCase(unittest.TestCase):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '//', 77.0, '-', 4.0, ')', '+', 2.0]))
 
     def test20(self):
-        self.assertEqual(True,
-                          lambda: calc_obj.validate_parsed_list(['(', 1.0, '+', 5.0, ')', '(', 1.0, '+', 5.0, ')']))
+        self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', 1.0, '+', 5.0, ')', '(',
+                                                                      1.0, '+', 5.0, ')']))
 
     def test21(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')', '.', 2.0]))
@@ -77,7 +76,7 @@ class ExpectedFailureTestCase(unittest.TestCase):
 
     def test23(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '//', 77.0, '-', 4.0, ')',
-                                                                             'sin', '(', 2.0, ')']))
+                                                                      'sin', '(', 2.0, ')']))
 
     def test24(self):
         self.assertEqual(True,
@@ -88,32 +87,30 @@ class ExpectedFailureTestCase(unittest.TestCase):
 
     def test26(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '//', 77.0, '-', 4.0,
-                                                                             ')', 'sin', '(', 2.0, ')']))
+                                                                      ')', 'sin', '(', 2.0, ')']))
 
     def test27(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')',
-                                                                             '.', '(', 2.0, '+', '1']))
+                                                                      '.', '(', 2.0, '+', '1']))
 
     def test28(self):
-        self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-',
-
-                                                                             4.0, '.', '.', ')']))
+        self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, '.', '.', ')']))
 
     def test29(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')',
-                                                                             '.', '(', 2.0, '+', '1']))
+                                                                      '.', '(', 2.0, '+', '1']))
 
     def test30(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')',
-                                                                             '.', ',', 2.0, '+', '1']))
+                                                                      '.', ',', 2.0, '+', '1']))
 
     def test31(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')',
-                                                                             '.', '-', 2.0, '+', '1']))
+                                                                      '.', '-', 2.0, '+', '1']))
 
     def test32(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')',
-                                                                             '.', 'sin', '(', 2.0, ')']))
+                                                                      '.', 'sin', '(', 2.0, ')']))
 
     def test33(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['(', '.', 77.0, '-', 4.0, ')', '.', 'e']))
@@ -208,7 +205,7 @@ class ExpectedFailureTestCase(unittest.TestCase):
 
     def test63(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list([15.0, '*', 25.0, '+', '(', 1.0,
-                                                                             '+', ')', 1.0, ')']))
+                                                                      '+', ')', 1.0, ')']))
 
     def test64(self):
         self.assertEqual(True, lambda: calc_obj.validate_parsed_list(['abs']))
@@ -219,115 +216,77 @@ class ExpectedSuccessTestCase(unittest.TestCase):
         self.assertEqual(False, calc_obj.validate_parsed_list(['-', 13.0]))
 
     def test2(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([6.0, '-', '(', '-',
-                                                        13.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list([6.0, '-', '(', '-', 13.0, ')']))
 
     def test3(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([1.0, '-', '-', '-',
-                                                        1.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '-', '-', '-', 1.0]))
 
     def test4(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['-', '+', '-', '-',
-                                                        '-', '+', '-', 1.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['-', '+', '-', '-', '-', '+', '-', 1.0]))
 
     def test5(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '+', 2.0,
-                                                                                                         '*', 2.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '+', 2.0, '*', 2.0]))
 
     def test6(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([1.0, '+', '(', 2.0, '+',
-                                                        3.0, '*', 2.0, ')', '*',
-                                                        3.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '+', '(', 2.0, '+', 3.0, '*', 2.0, ')', '*', 3.0]))
 
     def test7(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([10.0, '*', '(', 2.0,
-                                                        '+', 1.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list([10.0, '*', '(', 2.0, '+', 1.0, ')']))
 
     def test8(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([10.0, '^', '(', 2.0,
-                                                        '+', 1.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list([10.0, '^', '(', 2.0, '+', 1.0, ')']))
 
     def test9(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([100.0, '/', 3.0,
-                                                        '^', 2.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([100.0, '/', 3.0, '^', 2.0]))
 
     def test10(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([100.0, '/', 3.0, '%',
-                                                        2.0, '^', 2.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([100.0, '/', 3.0, '%', 2.0, '^', 2.0]))
 
     def test11(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['pi', '+', 'e']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['pi', '+', 'e']))
 
     def test12(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['log', '(', 'e', ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['log', '(', 'e', ')']))
 
     def test13(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['sin', '(', 'pi', '/',
-                                                        2.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['sin', '(', 'pi', '/', 2.0, ')']))
 
     def test14(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(['log10', '(',
-                                                                                                         100.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['log10', '(', 100.0, ')']))
 
     def test15(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            ['sin', '(', 'pi', '/', 2.0, ')', '*', 111.0, '*', 6.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['sin', '(', 'pi', '/', 2.0, ')', '*', 111.0, '*', 6.0]))
 
     def test16(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            [2.0, '*', 'sin', '(', 'pi', '/', 2.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list([2.0, '*', 'sin', '(', 'pi', '/', 2.0, ')']))
 
     def test17(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['abs', '(', '-', 5.0,
-                                                        ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['abs', '(', '-', 5.0, ')']))
 
     def test18(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(['round', '(',
-                                                                                                         123.456789,
-                                                                                                         ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['round', '(', 123.456789, ')']))
 
     def test19(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([102.0, '%', 12.0,
-                                                        '%', 7.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([102.0, '%', 12.0, '%', 7.0]))
 
     def test20(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([100.0, '/', 4.0, '/',
-                                                        3.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([100.0, '/', 4.0, '/', 3.0]))
 
     def test21(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([2.0, '^', 3.0, '^',
-                                                        4.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([2.0, '^', 3.0, '^', 4.0]))
 
     def test22(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            [1.0, '+', 2.0, '*', 3.0, '==', 1.0, '+', 2.0, '*', 3.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '+', 2.0, '*', 3.0, '==', 1.0, '+', 2.0, '*', 3.0]))
 
     def test23(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            ['e', '^', 5.0, '>=', 'e', '^', 5.0, '+', 1.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['e', '^', 5.0, '>=', 'e', '^', 5.0, '+', 1.0]))
 
     def test24(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            [1.0, '+', 2.0, '*', 4.0, '/', 3.0, '+', 1.0, '!=', 1.0, '+', 2.0, '*', 4.0, '/', 3.0, '+', 2.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '+', 2.0, '*', 4.0, '/', 3.0, '+', 1.0, '!=', 1.0,
+                                                               '+', 2.0, '*', 4.0, '/', 3.0, '+', 2.0]))
 
     def test25(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['(', 100.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['(', 100.0, ')']))
 
     def test26(self):
         self.assertEqual(False, calc_obj.validate_parsed_list([666.0]))
@@ -336,62 +295,54 @@ class ExpectedSuccessTestCase(unittest.TestCase):
         self.assertEqual(False, calc_obj.validate_parsed_list(['-', 0.1]))
 
     def test28(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([1.0, '/', 3.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '/', 3.0]))
 
     def test29(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([0.1, '*', 2.0, '^',
-                                                        56.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([0.1, '*', 2.0, '^', 56.0]))
 
     def test30(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['e', '^', 34.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['e', '^', 34.0]))
 
     def test31(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['(', 2.0, '^', '(', 'pi', '/', 'pi', '+', 'e',
-                                                        '/', 'e', '+', 2.0, '^', 0.0, ')', ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['(', 2.0, '^', '(', 'pi', '/', 'pi', '+', 'e', '/', 'e',
+                                                               '+', 2.0, '^', 0.0, ')', ')']))
 
     def test32(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['(', 2.0, '^', '(', 'pi', '/', 'pi', '+', 'e',
-                                                        '/', 'e', '+', 2.0, '^', 0.0, ')', ')',
-                                                        '^', '(', 1.0, '/', 3.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['(', 2.0, '^', '(', 'pi', '/', 'pi', '+', 'e', '/', 'e',
+                                                               '+', 2.0, '^', 0.0, ')', ')', '^', '(', 1.0, '/',
+                                                               3.0, ')']))
 
     def test33(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            [10.0, '*', 'e', '^', 0.0, '*', 'log10', '(', 0.4, '-', 5.0, '/', '-', 0.1, '-',
-             10.0, ')', '-', '-', 'abs', '(', '-', 53.0, '/', 10.0, ')', '+', '-', 5.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([10.0, '*', 'e', '^', 0.0, '*', 'log10', '(', 0.4, '-',
+                                                               5.0, '/', '-', 0.1, '-', 10.0, ')', '-', '-', 'abs', '(',
+                                                               '-', 53.0, '/', 10.0, ')', '+', '-', 5.0]))
 
     def test34(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            ['sin', '(', '-', 'cos', '(', '-', 'sin', '(', 3.0, ')', '-', 'cos',
-             '(', '-', 'sin', '(', '-', 3.0, '*', 5.0, ')', '-', 'sin', '(', 'cos', '(',
-             'log10', '(', 43.0, ')', ')', ')', ')', '+', 'cos', '(', 'sin', '(', 'sin', '(',
-             34.0, '-', 2.0, '^', 2.0, ')', ')', ')', ')', '-', '-', 'cos', '(', 1.0, ')', '-', '-',
-             'cos', '(', 0.0, ')', '^', 3.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['sin', '(', '-', 'cos', '(', '-', 'sin', '(', 3.0, ')',
+                                                               '-', 'cos', '(', '-', 'sin', '(', '-', 3.0, '*', 5.0,
+                                                               ')', '-', 'sin', '(', 'cos', '(', 'log10', '(', 43.0,
+                                                               ')', ')', ')', ')', '+', 'cos', '(', 'sin', '(', 'sin',
+                                                               '(', 34.0, '-', 2.0, '^', 2.0, ')', ')', ')', ')', '-',
+                                                               '-', 'cos', '(', 1.0, ')', '-', '-', 'cos', '(', 0.0,
+                                                               ')', '^', 3.0, ')']))
 
     def test35(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            [2.0, '^', '(', 2.0, '^', 2.0, '*', 2.0, '^', 2.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list([2.0, '^', '(', 2.0, '^', 2.0, '*', 2.0, '^', 2.0, ')']))
 
     def test36(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(
-            ['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^', 'sin', '(', 23.0, ')', ',', 45.0,
-             ')', '+', 'cos', '(', 3.0, '+', 'log10', '(', 'e', '^', '-', 'e', ')', ')', ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^',
+                                                               'sin', '(', 23.0, ')', ',', 45.0, ')', '+', 'cos', '(',
+                                                               3.0, '+', 'log10', '(', 'e', '^', '-', 'e', ')', ')',
+                                                               ')']))
 
     def test37(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list([1.0, '*', '-', 13.0]))
+        self.assertEqual(False, calc_obj.validate_parsed_list([1.0, '*', '-', 13.0]))
 
     def test38(self):
-        self.assertEqual(False,
-                         calc_obj.validate_parsed_list(['log', '(', 1.0, ',', '-', 13.0, ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['log', '(', 1.0, ',', '-', 13.0, ')']))
 
     def test39(self):
-        self.assertEqual(False, calc_obj.validate_parsed_list(['log', '(', 1.0, ',',
-                                                                                  '(', '-', 13.0, ')', ')']))
+        self.assertEqual(False, calc_obj.validate_parsed_list(['log', '(', 1.0, ',', '(', '-', 13.0, ')', ')']))
 
     # incorrect number of arguments processing in calc
     def test40(self):
