11d0b016ddc10ffe4583e20fce4b1b8b0f4624a2
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-06-05 10:55:00 +0300

    Refactor calculator’s formatters

--- final_task/pycalc/calculator/formatters.py
+++ final_task/pycalc/calculator/formatters.py
@@ -8,13 +8,13 @@ from .messages import ERROR_MSG_PREFIX
 ERROR_PLACE_INDICATOR = '^'
 
 
-def err_msg_formatter(msg):
+def prefix_err_msg(msg):
     """Return an error message with an error prefix."""
 
     return f'{ERROR_MSG_PREFIX}{msg}'
 
 
-def err_ctx_formatter(ctx):
+def ctx_formatter(ctx):
     """
     Return a two-line string with a source in the first line
     and a sign in the second one which indicate
@@ -25,3 +25,11 @@ def err_ctx_formatter(ctx):
     pos = ctx.pos
 
     return '{}\n{}{}'.format(source, ' ' * (pos), ERROR_PLACE_INDICATOR)
+
+
+def err_msg_with_ctx_formatter(msg, ctx):
+    """Return an error message with context information."""
+
+    ctx_msg = ctx_formatter(ctx)
+
+    return f'{msg}\n{ctx_msg}'
