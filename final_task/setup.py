999ed8b5580afa41c91aa611cda9dc38dea6e088
maximtsyba <MaksimTsuba@gmail.com>
2019-06-06 08:42:58 +0300

    fix setup

--- final_task/setup.py
+++ final_task/setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/javatechy/dokr",
     packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['pycalc = pycalc:main']},
     py_modules=["pycalc"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
