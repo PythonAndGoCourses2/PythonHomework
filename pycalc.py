c5446410a0effbe1e14a18c680943b7afae80883
lizasavanovich <liza.savanovich@mail.ru>
2019-06-06 23:44:11 +0300

    2222

--- pycalc.py
+++ pycalc.py
@@ -2,7 +2,6 @@ import math
 import operator
 import sys
 import argparse
-import setuptools
 
 """Module with calculator methods."""
 
@@ -299,23 +298,6 @@ def myresult(input_data):
 
 ca = Calculator()
 
-
-setuptools.setup(
-    name="pycalc",
-    version="0.1",
-    author="Lizaveta_Savanovich",
-    author_email="liza.savanovich@mail.ru",
-    description="Command-line calculator",
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    entry_points={'console_scripts': ['pycalc = pycalc:main']},
-    py_modules=['pycalc'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
 
 if __name__ == '__main__':
     main()
