1be437441cd7fc9e3713bd8998884cee4a42057c
lizasavanovich <liza.savanovich@mail.ru>
2019-06-06 22:24:37 +0300

    basic1

--- pycalc/setup.py
+++ pycalc/setup.py
@@ -7,8 +7,12 @@ setuptools.setup(
     author_email="liza.savanovich@mail.ru",
     description="Command-line calculator",
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
     entry_points={'console_scripts': ['pycalc = pycalc:main']},
     py_modules=['pycalc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
