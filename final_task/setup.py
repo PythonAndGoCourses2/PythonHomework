3b75d420e757b5b0f61c151fcf6f85ee04a4371e
Antossh <51364187+Antossh@users.noreply.github.com>
2019-06-06 19:22:16 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup, find_packages
+
 setup(
-	name="Pycalc",
-	version="1.0",
-	description="Pure-Python command line calculator. \n"
-				"Python training program kindly supported by EPAM Systems, Minsk.",
-	author="Antos Shakhbazau",
-	author_email="Shakhbazau@gmail.com",
-	python_requires='>=3.6',
-	packages=find_packages(),
-	entry_points={
-		'console_scripts': [
-			'pycalc = pycalc.launcher:main',
-		],
-	}
+    name='pycalc',
+    version='1.0',
+    author='Antos Shakhbazau',
+    author_email='Shakhbazau@gmail.com',
+    packages=find_packages(),
+    description='Pure-python command line calculator.',
+    py_modules=['pycalc'],
+    entry_points={'console_scripts': ['pycalc=pycalc:main', ], },
 )
